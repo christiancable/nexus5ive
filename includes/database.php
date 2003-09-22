bceb271e3178df9e71e8443f63f3ae036c869bfa
christiancable <christiancable>
2003-09-22 12:17:54 +0000

    removed site dependent defines - cfc

--- includes/database.php
+++ includes/database.php
@@ -8,18 +8,10 @@ putenv("TZ=GB");
 include('../phplib/php/template.inc');
 include('database_layer.php');
 include('interface_layer.php');
+include('site.php');
 
-// site specific stuff
-
+// defines
 define('TEMPLATE_HOME', '../templates/');
-define('DEFAULT_TEMPLATE', 'xp');
-define('SYSOP_NAME', "xxxx");
-define('SYSOP_MAIL', 'xxxx');
-define('SYSOP_ID','xxxx');
-define('MYSQL_USER', 'xxxx');
-define('MYSQL_PASSWORD','xxxx');
-define('MYSQL_SERVER','xxxx');
-define('MYSQL_DATABASE','xxxx');
 
 // functions
 function opendata()
