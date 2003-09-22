5009ee208bda30b16bab6715fd579d8d279b72e1
christiancable <christiancable>
2003-09-22 12:17:32 +0000

    site dependant - cfc

---
+++ includes/site.php
@@ -0,0 +1,17 @@
+<?php
+
+# this is for custom site defines
+
+
+define('DEFAULT_TEMPLATE', 'xp');
+define('SYSOP_NAME', "xxxx");
+define('SYSOP_MAIL', 'xxxx');
+define('SYSOP_ID','xxxx');
+define('MYSQL_USER', 'xxxx');
+define('MYSQL_PASSWORD','xxxx');
+define('MYSQL_SERVER','xxxx');
+define('MYSQL_DATABASE','xxxx');
+
+
+
+?>
