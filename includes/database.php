49e564bc1ee705c3b3bb02b01016fff9072416c0
christiancable <christiancable>
2003-11-05 13:07:19 +0000

    removed some rubbish -cfc

--- includes/database.php
+++ includes/database.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // non-visual functions
 // all database bits will eventuall move to database_layer.php
 // env
@@ -68,7 +68,7 @@ function is_message_owner($message_id, $user_id, $db)
 	$sql = "SELECT sectiontable.user_id FROM messagetable, topictable,sectiontable
            WHERE message_id=$message_id AND
            messagetable.topic_id=topictable.topic_id AND
-           sectiontable.section_id=topictable.section_id"; 
+           sectiontable.section_id=topictable.section_id";
 
 	$ownerinfo = mysql_query($sql);
 	$owner = mysql_result($ownerinfo, 0, "user_id");
@@ -162,10 +162,19 @@ function is_message_secret($message_id)
 function get_username($user_id)
 {
 	$sql = "SELECT user_name FROM usertable WHERE user_id=$user_id";
-	$usernameinfo = mysql_query($sql);
-	$userresult = mysql_result($usernameinfo, 0, "user_name");
 
-	return $userresult;
+	if(!$usernameinfo = mysql_query($sql)){
+		return false;
+	}
+
+	if(mysql_num_rows($usernameinfo)){
+		$userresult = mysql_result($usernameinfo, 0, "user_name");
+		return $userresult;
+	} else {
+		return false;
+	}
+
+
 } 
 
 #removed as this is not used - cfc
@@ -214,100 +223,27 @@ function count_messages_in_topic($topic_id)
 {
 	// returns the total amount of messages in a given topic
 	// returns false if any probs
-	
+
 	$sql = "SELECT COUNT(message_id) AS total_msg FROM messagetable WHERE topic_id=$topic_id";
 
 	if (!$message_info = mysql_query($sql)) {
 		return false;
-	} 
+	}
 
 	if (mysql_num_rows($message_info)) {
 		$message_array = mysql_fetch_array($message_info, MYSQL_ASSOC);
 		return $message_array[total_msg];
 	} else {
 		return false;
-	} 
-} 
+	}
+}
 
-function displaymessage($messagerow, $topic_id, $db, $user)
-{
-   
-    // displays a single topic message, this function shouldn't be used anymore ? - cfc
-    # currently only used in delpost.php	   
-    
-	$userinfo = mysql_query("SELECT * FROM usertable WHERE user_id=$messagerow[user_id]", $db);
-	$userrow = mysql_fetch_array($userinfo);
-
-	$sql = "SELECT  user_id FROM topictable,sectiontable WHERE topictable.section_id=sectiontable.section_id and topictable.topic_id=$topic_id";
-
-	$ownerinfo = mysql_query($sql);
-	if ($user) {
-		$ownerrow = mysql_fetch_array($ownerinfo);
-		$owner = $ownerrow[0];
-	} else {
-		$owner = "invalid";
-	} 
-
-	$sql = "SELECT section_id FROM topictable WHERE topic_id=$messagerow[topic_id]";
-	$sectioninfo = mysql_query($sql, $db);
-	$sectionrow = mysql_fetch_array($sectioninfo); 
-	// current section owner is now in owerrow[1]
-	$sql = "SELECT DATE_FORMAT(message_time,\"%a %b %D - %H:%i %Y\") FROM messagetable WHERE message_id=$messagerow[message_id]";
-	$dateinfo = mysql_query($sql, $db);
-	$daterow = mysql_fetch_array($dateinfo);
-
-	topicheader();
-
-	echo "<font color=" . COLOUR_HIGHLIGHT . ">";
-	if (is_message_secret($messagerow["message_id"], $user, $db)) {
-		if (is_message_owner($messagerow["message_id"], $user, $db)) {
-			echo "<b>Secret: </b></font>(" . $messagerow["message_popname"];
-			echo ") <a href=\"myinfo.php?section_id=$sectionrow[0]&lookat=$userrow[0]\">$userrow[1]</a>";
-			echo " <br>";
-		} else {
-			echo "<b>From:</b></font> ( Popname Hidden ) Author Hidden";
-			echo "<br>";
-		} 
-	} else {
-		echo "<b>From: </b></font>(" . $messagerow["message_popname"];
-		echo ") <a href=\"myinfo.php?section_id=$sectionrow[0]&lookat=$userrow[0]\">$userrow[1]</a>";
-		echo " <br>";
-	} 
-
-	echo "<font color=" . COLOUR_HIGHLIGHT . ">";
-	echo "<b>Date: </b></font>$daterow[0]<br>";
-
-	echo "<font color=" . COLOUR_HIGHLIGHT . ">";
-	if (strlen($messagerow["message_title"])) {
-		echo "<b>Subject:</b> </font>" . $messagerow["message_title"];
-	} else {
-		echo "</font>";
-	} 
-	// echo "</font>";
-	if (is_message_owner($messagerow["message_id"], $user, $db)) {
-		echo "<a href=\"delpost.php?message_id=$messagerow[message_id]&topic_id=$topic_id\"><br>[ remove ]</a>";
-		echo " -- <a href=\"alterpost.php?message_id=$messagerow[message_id]&topic_id=$topic_id\">[ edit ]</a>";
-	} 
-	topicfooter();
-
-	?>
-         <table bgcolor=<?php echo COLOUR_MSG_BK ?> align="center"  border="0" width="100%"
-         cellpadding="4" cellspacing="0"><tr><td width="100%"><font face="<?php echo FONT_FACE ?>">
-         <?php
-	echo "<font face=\"" . FONT_FACE . "\">" . nx_code($messagerow["message_text"]) . "</font>";
-
-	?>
-          </font></td></tr></table>
-          <?php
-
-} 
 
 function displaytopic($topicrow, $db, $user)
-{ 
+{
          # update this to use templates!
 	 # this is used in deltopic.php and section.php - cfc
-	 
-	// topicheader();
+
 	echo "\n\n\n";
 	echo '<table width="100%"><tr><td>';
 	$new_msg = new_messages_in_topic($topicrow[0], $user);
@@ -316,8 +252,8 @@ function displaytopic($topicrow, $db, $user)
 		echo "<a href=\"readtopic.php?section_id=$topicrow[2]&topic_id=$topicrow[0]\">";
 		echo "<img src=\"images/xp/star.gif\" alt=\"*\" border=\"0\">$topicrow[1]<img src=\"images/xp/star.gif\" alt=\"*\" border=\"0\">";
 		echo "</FONT></a></b>";
-		echo "<blockquote>$topicrow[3]<br>"; 
-		// echo "<br><br><font size=\"-1\" color=".COLOUR_HIGHLIGHT."><b>$new_msg</b> New Messages</font><br>";
+		echo "<blockquote>$topicrow[3]<br>";
+		
 	} else {
 		echo "<a href=\"readtopic.php?section_id=$topicrow[2]&topic_id=$topicrow[0]\">";
 		echo "<FONT  size=+1><b>$topicrow[1]</b></font></a>";
@@ -329,8 +265,7 @@ function displaytopic($topicrow, $db, $user)
 		echo "<a href=\"altertopic.php?topic_id=$topicrow[0]\">[ edit ]</a>";
 		echo" --- <a href=\"deltopic.php?topic_id=$topicrow[0]\">[ delete ]</a> -- ";
 	} 
-	// if( (strlen($topicrow[1]))  && ($new_msg!=-1) )
-	// echo"<a href=\"unsub.php?section_id=$topicrow[2]&topic_id=$topicrow[0]\">[ unsubscribe ]</a>";
+
 	if (!unsubscribed_from_topic($topicrow[0], $_SESSION[current_id]))
 		echo"<a href=\"unsub.php?section_id=$topicrow[2]&topic_id=$topicrow[0]\">[ unsubscribe ]</a>";
 
@@ -345,17 +280,17 @@ function sectionheader($myrow)
 
 	$user_array = get_user_array($current_id);
 
-	echo "<h2><a href=\"section.php?section=" . $myrow["section_id"];
+	echo "<font size=+2><b><a href=\"section.php?section=" . $myrow["section_id"];
 
 	if (new_messages_in_section($current_id, $myrow[section_id])) {
-		echo "\">" . SECTION_GRAPHIC_MSG . $myrow["section_title"] . "</a></h2>";
+		echo "\">" . SECTION_GRAPHIC_MSG . $myrow["section_title"] . "</a></b></font>";
 	} else {
-		echo "\">" . SECTION_GRAPHIC . $myrow["section_title"] . "</a></h2>";
-	} 
-
+		echo "\">" . SECTION_GRAPHIC . $myrow["section_title"] . "</a></b></font>";
+	}
+	echo '<br><small style="color: rgb(129, 129, 129);"><span style="font-style: italic;">'.$myrow[section_intro].'</span></small><br>';
 	if ($num = get_count_section_messages($myrow[section_id])) {
 		$sql = "SELECT messagetable.message_id FROM messagetable, topictable, topicview WHERE messagetable.topic_id = topictable.topic_id AND topictable.section_id =" . $myrow["section_id"] . " AND topicview.user_id =$current_id AND messagetable.message_time > topicview.msg_date AND topicview.topic_id = messagetable.topic_id";
-		echo "<font size=-2>" . $num . " messages</font>";
+		echo "<font size=-2> " . $num . " messages</font>";
 	} 
 	// add section owner functions references here
 	if (can_user_edit_section($user_array, $myrow)) { // # ADD moderator here too!
@@ -456,42 +391,14 @@ function new_messages_in_topic($topic_id, $user_id)
 	return $new_messages;
 } 
 
-# removed as this is not used - cfc
-/*
-function get_score($user_id)
-{
-	$sql = "SELECT user_totalvisits, user_totaledits FROM usertable WHERE user_id=$user_id";
-	if (!$user_results = mysql_query($sql)) {
-		nexus_error();
-	} 
-	$user_row = mysql_fetch_array($user_results);
-	if ($user_row["user_totalvisits"]) {
-		$user_score = round($user_row["user_totaledits"] / $user_row["user_totalvisits"] + $user_row["user_totaledits"] / 1000);
-	} else {
-		$user_score = 0;
-	} 
-	// now find out the score from being an owner
-	// er total edits in sections div 2000 I think should be fair
-	$sql = "SELECT count(messagetable.message_id) FROM messagetable, topictable, sectiontable WHERE messagetable.topic_id = topictable.topic_id AND topictable.section_id = sectiontable.section_id and sectiontable.user_id=$user_id";
-
-	if (!$section_results = mysql_query($sql)) {
-		nexus_error();
-	} 
-	// $section_row = mysql_fetch_array($section_results);
-	$section_score = mysql_result($section_results, 0);
-
-	$score = $user_score + round($section_score / 2000);
-
-	return $score;
-} 
-*/
+#
 
 function nx_code($text){
 
 	# regular expressions ROX	
 	# can we check for http here somehow? - cfc
 	
-	$pattern ="#\[PICTURE\-\](.*)\[\-PICTURE\]#is";
+	$pattern ="/\[PICTURE\-\](.*)\[\-PICTURE\]/Ui";
 
 	# check to see if the user has turned off pictures here 
 	#if user has pictures turned off
@@ -503,19 +410,19 @@ function nx_code($text){
 	}
 	$text = preg_replace($pattern, $replacement, $text);
 	
-	$pattern ="#\[WWW\-\](.*)\[\-WWW\]#is";
+	$pattern ="/\[WWW\-\](.*)\[\-WWW\]/Ui";
 	$replacement = '<a href="'."$1".'" target="_blank">['."$1".']</a>';
 	$text = preg_replace($pattern, $replacement, $text);
 	
-	$pattern ="#\[I\-\](.*)\[\-I\]#is";
+	$pattern ="#\[I\-\](.*)\[\-I\]#isU";
 	$replacement = '<I>'."$1".'</I>';
 	$text = preg_replace($pattern, $replacement, $text);
 	
-	$pattern ="#\[B\-\](.*)\[\-B\]#is";
+	$pattern ="#\[B\-\](.*)\[\-B\]#isU";
 	$replacement = '<B>'."$1".'</B>';
 	$text = preg_replace($pattern, $replacement, $text);
 	
-	$pattern ="#\[ASCII\-\](.+?)\[\-ASCII\]#is";
+	$pattern ="#\[ASCII\-\](.+?)\[\-ASCII\]#isU";
 	$replacement = '<pre> '."$1".'</pre>';
     	$text = preg_replace($pattern, $replacement, $text);
 	
