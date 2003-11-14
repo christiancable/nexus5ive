67b17a98cc248391550b9fd7e7a85037042243d9
christiancable <christiancable>
2003-11-14 08:54:52 +0000

    removed some themes.php references - cfc

--- web/section.php
+++ web/section.php
@@ -6,30 +6,22 @@ $db = opendata();
 session_start();
 $template_location = TEMPLATE_HOME . $my_theme; 
 // check login
+
 if (!validlogin()) {
     eject_user();
 } 
 // ADD CHECKS HERE
 $user_array = get_user_array($_SESSION[current_id]);
+
 // temp - transsistional vars
 if (isset($section_id))
     $section = $section_id;
 // end temp
-if (!$sectioninfo = mysql_query("SELECT section_id, section_title, user_id, parent_id FROM sectiontable WHERE section_id=$section", $db)) {
-    nexus_error();
-} 
 
-if (!mysql_num_rows($sectioninfo)) {
-    // if the section does not exist then jump to main menu
-    // update this to jump to section zero I guess
-    // UPDATE THIS
-    header("Location: http://" . $_SERVER['HTTP_HOST'] . "/section.php?section=1");
-    exit();
-} 
 
-if (!$sectioninfodetails = mysql_fetch_array($sectioninfo)) {
+if (!$sectioninfodetails = get_section($section)) {
     nexus_error();
-} 
+}
 
 $sectionname = $sectioninfodetails["section_title"];
 $sectionowner = $sectioninfodetails["user_id"];
@@ -38,19 +30,13 @@ $sectionparent = $sectioninfodetails["parent_id"];
 $breadcrumbs = get_breadcrumbs($section);
 
 $location_str = '<a href="section.php?section=' . $sectioninfodetails[section_id] . '">' . $sectionname . '</a>';
-echo "<!-- $location_str -->";
-// $location_str = $sectionname;
-update_location($location_str); 
-// update_location($sectionname);
-if (!$ownernameinfo = mysql_query("SELECT user_name FROM usertable where user_id=$sectionowner")) {
-    nexus_error();
-} 
 
-if (mysql_num_rows($ownernameinfo)) {
-    $ownername = mysql_result($ownernameinfo, 0, "user_name");
-} else {
-    $ownername = "unknown moderator";
-} 
+
+update_location($location_str);
+
+if(!$ownername = get_username($sectionowner)){
+	$ownername = "unknowm moderator";
+}
 
 $t = new Template($template_location);
 // BEGIN DISPLAY TOPIC TITLE
@@ -81,7 +67,6 @@ $t->set_var("user_id", $_SESSION[current_id]);
 $t->pparse("MyFinalOutput", "WholePage");
 // END DISPLAY TOPIC TITLE
 // ## DO LINKS TEMPLATE
-$topicinfo = mysql_query("SELECT * FROM topictable  WHERE topictable.section_id=$section ORDER BY topic_weight", $db);
 // ## END LINKS TEMPLATE
 // BEGIN DISPLAY TOP SET OF BUTTONS
 if (is_section_owner($sectioninfodetails[section_id], $user_array[user_id], $db)) {
@@ -89,60 +74,103 @@ if (is_section_owner($sectioninfodetails[section_id], $user_array[user_id], $db)
 } else {
     $t->set_file('top_links', 'menu_topic_links.html');
 } 
-// $t->set_block('top_links','PARENT_LINKS','');
-$t->set_var("section_id", $section);
 
+$t->set_var("section_id", $section);
 $t->pparse('content', 'top_links');
 // END DISPLAY TOP SET OF BUTTONS
-// echo "DEBUG session user id is ".$_SESSION[current_id]."<br>";
+
 // ## topics
-if ($myrow = mysql_fetch_array($topicinfo)) {
-    do {
-        displaytopic($myrow, $db, $current_id); 
-        // #debug
-        // echo "<b>DEBUG unsubbed is ".unsubscribed_from_topic($myrow[topic_id],$_SESSION[current_id])." </b>";
-        // #end debug
-    } while ($myrow = mysql_fetch_array($topicinfo));
-} else {
-} 
+
+
+if ($topics_list = get_section_topics($section) ) {
+   		
+	foreach ($topics_list as $current_topic_array){
+
+		if(new_messages_in_topic($current_topic_array[topic_id], $_SESSION[current_id])){
+
+			if(can_user_edit_topic($user_array, $current_topic_array)){
+
+				if(!unsubscribed_from_topic($current_topic_array[topic_id], $_SESSION[current_id])){
+					$mode = "NEW_ADMIN_SUB";
+				} else {
+					$mode = "NEW_ADMIN_UNSUB";
+
+				}
+
+			} else {
+
+				if(!unsubscribed_from_topic($current_topic_array[topic_id], $_SESSION[current_id])){
+					$mode = "NEW_NORMAL_SUB";
+				} else {
+					$mode = "NEW_NORMAL_UNSUB";
+
+				}
+			}
+
+		} else {
+
+			if(can_user_edit_topic($user_array, $current_topic_array)){
+
+				if(!unsubscribed_from_topic($current_topic_array[topic_id], $_SESSION[current_id])){
+					$mode = "ADMIN_SUB";
+				} else {
+					$mode = "ADMIN_UNSUB";
+
+				}
+
+			} else {
+
+				if(!unsubscribed_from_topic($current_topic_array[topic_id], $_SESSION[current_id])){
+					$mode = "NORMAL_SUB";
+				} else {
+					$mode = "NORMAL_UNSUB";
+
+				}
+			}
+
+
+
+
+		}
+
+	display_topic($current_topic_array, $_SESSION[current_id], $t, $mode);
+
+	}
+
+}
+
+
 // ########## subsections
-$sql = "SELECT * FROM sectiontable where parent_id = $section ORDER BY section_weight";
 
-$sectioninfo = mysql_query($sql, $db);
-
-$sectioninfo = mysql_query($sql, $db);
-if (mysql_num_rows($sectioninfo)) {
+$subsectionlist = get_subsectionlist_array($section);
+echo "<!-- looking for subsections of $section_id section -->";
+if ($subsectionlist) {
     // table begin
     $count = 0;
 
-    ?>
-		<TABLE width="100%" border="0">
-		<?php
-    if ($myrow = mysql_fetch_array($sectioninfo))
-        do {
-        if ($count % 2 == 0) {
-            echo "<TR VALIGN=TOP>";
-        } 
+    echo '<TABLE width="100%" border="0">';
 
-        echo "<TD>";
-        sectionheader($myrow); 
+    foreach ($subsectionlist as $current_sub_section_array){
+	if ($count % 2 == 0) {
+		echo "<TR VALIGN=TOP>";
+		}
+	echo "<TD>";
+        sectionheader($current_sub_section_array);
         // }
         echo "</b></font></TD>";
         $count ++;
-    } while ($myrow = mysql_fetch_array($sectioninfo));
-
+    }
     if ($count % 2) {
         echo "<TD></TD>";
-    } 
+    }
     // table end
     // check if we need a filler cell
-    ?>
-			  </TABLE>
-			  <?php
+    echo "</TABLE>";
 } else {
-    // printf("<br><b> no sections yet ! </b><br>");
-} 
-// # DO LINKS TEMPLATE
+    // no subsections
+}
+
+
 // BEGIN DISPLAY TOP SET OF BUTTONS
 if (is_section_owner($sectioninfodetails[section_id], $user_array[user_id], $db)) {
     $t->set_file('bottom_links', 'menu_menu_links_admin.html');
