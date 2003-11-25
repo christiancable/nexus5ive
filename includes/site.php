b60c7c828ff49d5a2267341673914a7cf904c137
christiancable <christiancable>
2003-11-25 13:52:04 +0000

    changed default theme to crystal - cfc

--- includes/site.php
+++ includes/site.php
@@ -3,7 +3,7 @@
 # this is for custom site defines
 
 
-define('DEFAULT_TEMPLATE', 'xp');
+define('DEFAULT_TEMPLATE', 'crystal');
 define('SYSOP_NAME', "xxxx");
 define('SYSOP_MAIL', 'xxxx');
 define('SYSOP_ID','xxxx');
