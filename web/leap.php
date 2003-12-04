29245b8b24caa1fd37c2373621c24539c1e29df1
christiancable <christiancable>
2003-12-04 14:31:19 +0000

    removed sql farming out the functionality to database_layer.php - cfc

--- web/leap.php
+++ web/leap.php
@@ -1,59 +1,27 @@
 <?php
-/* 
-
-leaps to the next section containg unread messages or to main menu
-
-update log
-2 Nov 2001 - updated function to use templates - xian
-
-todo:
-do not leap to unsubbed files
-do not leap to files we do not have the privs to read
-do not leap to files in sections we are banned from
+/*
+leap to a section with an unread topic OR leap to the main menu
 */
 
 //includes
-include('../includes/theme.php');
 include('../includes/database.php');
 
 
 //common stuff
 $db = opendata();
 session_start();
-#$template_location =TEMPLATE_HOME.$my_theme; 
 
 // check login
 if (!validlogin()){
-	eject_user();	
-}
-
-#get all section_ids, loop tho them all, break when new messages found and leap to seaction
-
-$sql = "SELECT section_id  FROM sectiontable";
-
-
-if(!$unreadsection = mysql_query($sql)){
-	nexus_error();
+	eject_user();
 }
 
 
-if($current_section = mysql_fetch_array($unreadsection)){
- do {
-
-		if(new_messages_in_section($_SESSION[current_id],$current_section[section_id])){
-		// leap to section
-		// exit
-		
-		header("Location: http://".$_SERVER['HTTP_HOST']."/section.php?section=$current_section[section_id]");
-		exit;
-		}
- 	  
-    } while ($current_section=mysql_fetch_array($unreadsection));
+if($target = find_next_unread_topic($_SESSION[current_id])){
 
 } else {
-	// main menu
+	$target = 1;
 }
 
-// leap to main menu
-header("Location: http://".$_SERVER['HTTP_HOST']."/section.php?section=1");
+header("Location: http://".$_SERVER['HTTP_HOST']."/section.php?section=$target");
 ?>
