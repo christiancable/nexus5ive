bd2bf5f7976a4db8ee7284d3503075883e459c87
christiancable <christiancable>
2003-12-05 08:35:57 +0000

    moved sql to database_layer - cfc

--- web/logout.php
+++ web/logout.php
@@ -1,25 +1,17 @@
 <?php
-//logs the user out, sets their location to Logout
-// last update May 22 2002
 
-include('../includes/theme.php');
 include('../includes/database.php');
 
 
 $db = opendata();
 session_start();
-$sql = 'UPDATE usertable SET user_status="Offline" WHERE user_id='.$current_id;
-if(!mysql_query($sql)){
- nexus_error();
-}
 
-# I had this session_destroy commented out before, but can't remember why as it's needed
-# will have to keep an eye on this bit I guess - cfc
+setuser_offline($_SESSION[current_id]);
 
 session_destroy();
+
 header("Location: http://".$_SERVER['HTTP_HOST']."/");
 exit;
-
 ?>
 
 
