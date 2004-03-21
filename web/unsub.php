432e0708a7180476a46ecbb70bf2045240f6d04d
christiancable <christiancable>
2004-03-21 14:00:35 +0000

    general tidy up

--- web/unsub.php
+++ web/unsub.php
@@ -1,27 +1,26 @@
 <?php
-# leaps to the next section containg unread messages or to main menu
-
+// unsubscribe user from topic
 // includes
 
 include_once('../includes/common.php');
 include_once('../includes/database_layer.php');
-# include_once('../includes/interface_layer.php');
 include_once('../includes/site.php');
 
-
 // parameters
-$section_id = $_GET[section_id];
-$topic_id = $_GET[topic_id];
+
+$section_id = $_GET['section_id'];
+$topic_id = $_GET['topic_id'];
 
 $db = opendata();
 session_start();
-
 // check login
-if (!validlogin()){
-	eject_user();	
+if (!validlogin())
+{
+  eject_user();	
 }
+!unsubscribe_from_topic($topic_id, $_SESSION['current_id']);
 
-unsubscribe_from_topic($topic_id, $_SESSION[current_id]);
+
 
 header("Location: http://".$_SERVER['HTTP_HOST'].get_bbsroot()."section.php?section_id=$section_id");
 ?>
