bfa701b8cf4da84bb878a5c76612902bddc2765e
Christian Cable <christiancable@gmail.com>
2015-08-07 11:11:45 +0100

    Removing old files to make way for Laravel re-write

--- pushtolive.sh
+++
@@ -1,5 +0,0 @@
-rsync -avz includes fraggle@www.nexus5.org.uk:nexus/includes
-rsync -avz templates fraggle@www.nexus5.org.uk:nexus/templates
-rsync -avz web fraggle@www.nexus5.org.uk:nexus/public_html
-rsync -avz vendor fraggle@www.nexus5.org.uk:nexus/vendor
-rsync -avz views fraggle@www.nexus5.org.uk:nexus/views
