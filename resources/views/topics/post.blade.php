30014140e6c12e88ff87caf7748d2428f78622ac
Christian Cable <christiancable@gmail.com>
2015-08-16 19:02:29 +0100

    Post view - made the dates human-friendly

--- resources/views/topics/post.blade.php
+++ resources/views/topics/post.blade.php
@@ -1,12 +1,12 @@
 <section>
     <ul>
         <li>From: <a href="{{ url("/users/{$post->author->user_name}") }}">{{$post->author->user_name}}</a> ({{$post->message_popname}})</li>
-        <li>Date: {{$post->message_time}}</li>
+        <li>Date: {{ date('D, F jS Y - H:i', strtotime($post->message_time)) }}</li>
         @unless (str_is($post->message_title, ""))
             <li>Subject: {{$post->message_title}}</li>
         @endunless
     </ul>
-    <p>{{$post->message_text}}</p>
+    <p>{!! nl2br($post->message_text) !!}</p>
     {{-- <li>{{$post->message_html}}</li> --}}
     {{-- <li>{{$post->update_user_id}}</li> --}}
 </section>
