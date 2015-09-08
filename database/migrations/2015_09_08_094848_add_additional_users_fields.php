d7f6821160e91d69d3002eb523a563a6c36363de
Christian Cable <christiancable@gmail.com>
2015-09-08 13:58:21 +0100

    migration for additional user fields from nexus

--- database/migrations/2015_09_08_094848_add_additional_users_fields.php
+++ database/migrations/2015_09_08_094848_add_additional_users_fields.php
@@ -17,7 +17,18 @@ class AddAdditionalUsersFields extends Migration
 			$table->string('popname')->nullable()->default('New User');
 			$table->mediumText('about')->nullable();
 			$table->string('location')->nullable()->default('Someplace');
-			$table->boolean('administrator')->default(false);
+			$table->boolean('administrator')->default(false);		
+			$table->boolean('banned')->default(false);
+			$table->boolean('deleted')->default(false);
+			$table->integer('totalVisits')->default(0);
+			$table->integer('totalPosts')->default(0);
+			$table->string('favouriteMovie')->nullable();
+			$table->string('favouriteMusic')->nullable();
+			$table->boolean('private')->default(true);
+			$table->string('ipaddress')->nullable()->default('127.0.0.1');
+			$table->string('currentActivity')->nullable();
+			$table->timestamp('latestLogin')->nullable();
+			
             //
         });
     }
@@ -35,6 +46,16 @@ class AddAdditionalUsersFields extends Migration
 			$table->dropColumn('about');
 			$table->dropColumn('location');
 			$table->dropColumn('administrator');
+			$table->dropColumn('banned');
+			$table->dropColumn('deleted');			
+			$table->dropColumn('totalVisits');
+			$table->dropColumn('totalPosts');
+			$table->dropColumn('favouriteMusic');
+			$table->dropColumn('favouriteMovie');
+			$table->dropColumn('private');
+			$table->dropColumn('ipaddress');
+			$table->dropColumn('currentActivity');
+			$table->dropColumn('latestLogin');
             //
         });
     }
