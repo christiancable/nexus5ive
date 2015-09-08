1bc74cd250bccc5542c743bb2e9703b01ff6a7f2
Christian Cable <christiancable@gmail.com>
2015-09-08 10:50:08 +0100

    migrations for adding old nexus fields to Users table - WIP

---
+++ database/migrations/2015_09_08_094848_add_additional_users_fields.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAdditionalUsersFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Users', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Users', function (Blueprint $table) {
+            //
+        });
+    }
+}
