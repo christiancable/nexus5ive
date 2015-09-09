06654effd5da6bfe8b1d4a48c63af5bf31d4b650
Christian Cable <christiancable@gmail.com>
2015-09-09 14:10:58 +0100

    Nexus/User to User fix for posting comments

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -1,6 +1,6 @@
 {!! Form::open(['url' => 'comments']) !!}
-{!! Form::hidden('user_id', $user->user_id) !!}
-{!! Form::hidden('redirect_user', $user->user_name) !!}
+{!! Form::hidden('user_id', $user->id) !!}
+{!! Form::hidden('redirect_user', $user->username) !!}
     <div class="form-group">
         {!! Form::label('text', 'Leave Comment') !!}
         {!! Form::text('text', null, ['class'=> 'form-control']) !!}
