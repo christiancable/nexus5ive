1dbec9ffc834f3bef5731924128de340b8e86f8d
Christian Cable <christiancable@gmail.com>
2015-09-19 18:38:05 +0100

    Updated main navigation
    * removed links to functions which are not there yet
    * make it stick to the top

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,36 +11,51 @@
 <body>
 
 
-<div class="navbar navbar-default">
-      <div class="container">
-        <div class="navbar-header">
-          <a href="/" class="navbar-brand">Home</a>
-          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        </div>
-        <div class="navbar-collapse collapse" id="navbar-main">
-          <ul class="nav navbar-nav">
-        <li><a href="#">Topic Leap</a></li>
-        <li><a href="#">Who's Online</a></li>
-        <li><a href="/users/">Examine User</a></li>      
-          </ul>
-
-          <ul>
-             <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
-            <span class="icon-bar">1</span>
-            <span class="icon-bar">2</span>
-            <span class="icon-bar">3</span>
-          </button>
-          </ul>
-
-        </div>
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">NexusFive</a>
       </div>
+      <div id="navbar" class="navbar-collapse collapse">
+
+       @if (Auth::check())
+          @if ($authUser = Auth::user())
+
+            <ul class="nav navbar-nav">
+              <li {{-- class="active" --}}><a href="/">Home</a></li>
+              <li><a href="/users/">Examine User</a></li>
+              {{-- <li><a href="#contact">Contact</a></li> --}}
+    {{--           <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li> --}}
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="/auth/logout">Logout</a></li>
+              {{-- <li><a href="../navbar-static-top/">Static top</a></li> --}}
+              {{-- <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li> --}}
+            </ul>
+        
+          @endif
+        
+        @endif
+      </div><!--/.nav-collapse -->
     </div>
-
-
+  </nav>
 
 <div class="container">
     <ol class="breadcrumb">
