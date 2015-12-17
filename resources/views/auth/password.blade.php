093141c07a1b285995b5e5e225c977d70ac63514
Christian Cable <christiancable@gmail.com>
2015-12-17 20:14:14 +0000

    Password reset views - added in bootstrap container div

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,34 +1,36 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="col-md-6">
+<div class="container">
+  <div class="col-md-6">
 
-{!! Form::open(array('url' => '/password/email', 'class' => 'form')) !!}
+    {!! Form::open(array('url' => '/password/email', 'class' => 'form')) !!}
 
-<h1>Recover Your Password</h1>
+    <h1>Recover Your Password</h1>
 
-@if (count($errors) > 0)
-  <div class="alert alert-danger">
-    There were some problems recovering your password:
-    <br />
-    <ul>
+    @if (count($errors) > 0)
+    <div class="alert alert-danger">
+      There were some problems recovering your password:
+      <br />
+      <ul>
         @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
+        <li>{{ $error }}</li>
         @endforeach
-    </ul>
+      </ul>
+    </div>
+    @endif
+
+    <div class="form-group">
+      {!! Form::label('email', 'Your E-mail Address') !!}
+      {!! Form::text('email', null, 
+      array('class'=>'form-control', 'placeholder'=>'E-mail')) !!}
+    </div>
+
+    <div class="form-group">
+      {!! Form::submit('E-mail Password Reset Link', 
+      array('class'=>'btn btn-primary')) !!}
+    </div>
+    {!! Form::close() !!}
   </div>
-@endif
-
-<div class="form-group">
-  {!! Form::label('email', 'Your E-mail Address') !!}
-  {!! Form::text('email', null, 
-    array('class'=>'form-control', 'placeholder'=>'E-mail')) !!}
-</div>
-
-<div class="form-group">
-  {!! Form::submit('E-mail Password Reset Link', 
-    array('class'=>'btn btn-primary')) !!}
-</div>
-{!! Form::close() !!}
 </div>
 @endsection
--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -1,51 +1,53 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="col-md-6">
+<div class="container">
+  <div class="col-md-6">
 
 
 
-{!! Form::open(array('url' => '/password/reset', 'class' => 'form')) !!}
+    {!! Form::open(array('url' => '/password/reset', 'class' => 'form')) !!}
 
     <input type="hidden" name="token" value="{{ $token }}">
     {!! csrf_field() !!}
 
-<h1>Reset Your Password</h1>
+    <h1>Reset Your Password</h1>
 
-@if (count($errors) > 0)
-  <div class="alert alert-danger">
-    There were some problems resetting your password:
-    <br />
-    <ul>
+    @if (count($errors) > 0)
+    <div class="alert alert-danger">
+      There were some problems resetting your password:
+      <br />
+      <ul>
         @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
+        <li>{{ $error }}</li>
         @endforeach
-    </ul>
-  </div>
-@endif
+      </ul>
+    </div>
+    @endif
 
-<div class="form-group">
-  {!! Form::label('email', 'Your E-mail Address') !!}
-  {!! Form::text('email', null, 
-    array('class'=>'form-control', 'placeholder'=>'E-mail')) !!}
-</div>
+    <div class="form-group">
+      {!! Form::label('email', 'Your E-mail Address') !!}
+      {!! Form::text('email', null, 
+      array('class'=>'form-control', 'placeholder'=>'E-mail')) !!}
+    </div>
 
-<div class="form-group">
- {!! Form::label('password', 'New Password') !!}
- {!! Form::password('password', 
-   array('class'=>'form-control', 'placeholder'=>'Password')) !!}
-</div>
+    <div class="form-group">
+     {!! Form::label('password', 'New Password') !!}
+     {!! Form::password('password', 
+     array('class'=>'form-control', 'placeholder'=>'Password')) !!}
+   </div>
 
-<div class="form-group">
- {!! Form::label('password_confirmation', 'Confirm Password') !!}
- {!! Form::password('password_confirmation', 
-   array('class'=>'form-control', 'placeholder'=>'Password')) !!}
-</div>
+   <div class="form-group">
+     {!! Form::label('password_confirmation', 'Confirm Password') !!}
+     {!! Form::password('password_confirmation', 
+     array('class'=>'form-control', 'placeholder'=>'Password')) !!}
+   </div>
 
-<div class="form-group">
-  {!! Form::submit('Reset Password', 
+   <div class="form-group">
+    {!! Form::submit('Reset Password', 
     array('class'=>'btn btn-primary')) !!}
+  </div>
+  {!! Form::close() !!}
 </div>
-{!! Form::close() !!}
 </div>
 @endsection
