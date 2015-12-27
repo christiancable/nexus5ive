48363eebcc8cd67c360267832e7c528386d76782
Christian Cable <christiancable@gmail.com>
2015-12-27 14:58:59 +0000

    Adding in migration missed from last commit

---
+++ database/migrations/2015_12_27_123440_create_views_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateViewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('views', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('user_id')->unsigned();
+            $table->integer('topic_id')->unsigned();
+
+             $table->foreign('user_id')
+                ->references('id')
+                ->on('users')->onDelete('cascade');
+
+            $table->foreign('topic_id')
+                ->references('id')
+                ->on('topics')->onDelete('cascade');
+
+            $table->timestamp('latest_view_date');
+            $table->boolean('unsubscribed')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('views');
+    }
+}
