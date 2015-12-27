7fab0843518403fc01d492a6c8eb000322a76504
Christian Cable <christiancable@gmail.com>
2015-12-27 15:26:19 +0000

    PSR-2 fix

--- database/migrations/2015_12_27_123440_create_views_table.php
+++ database/migrations/2015_12_27_123440_create_views_table.php
@@ -18,7 +18,7 @@ class CreateViewsTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->integer('topic_id')->unsigned();
 
-             $table->foreign('user_id')
+            $table->foreign('user_id')
                 ->references('id')
                 ->on('users')->onDelete('cascade');
 
