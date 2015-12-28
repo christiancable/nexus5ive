67d9f5c4ff3c62f58529e1436e8bc0e908bf5ea0
Christian Cable <christiancable@gmail.com>
2015-12-28 20:20:46 +0000

    Switching off registrations for now...

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,8 +12,8 @@
 */
 
 // authentication
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+// Route::get('auth/register', 'Auth\AuthController@getRegister');
+// Route::post('auth/register', 'Auth\AuthController@postRegister');
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
