0981baae0dcc8fc3bad2af0d597e88ec2eb6cf38
Christian Cable <christiancable@gmail.com>
2015-12-29 13:24:08 +0000

    Section Layout: moved topics into view partial

--- resources/views/sections/index.blade.php
+++ resources/views/sections/index.blade.php
@@ -29,24 +29,9 @@
 
     <div class="content">
 
-
         @if (count($section->topics))
         @foreach ($section->topics as $topic)
-        <div class="well">
-            <h2>
-                @if ($topic->unreadPosts(Auth::user()->id))
-                <span class="glyphicon glyphicon-fire text-danger" aria-hidden="true"></span>
-                @else
-                <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
-                @endif
-
-                <a href="{{ action('Nexus\TopicController@show', ['topic_id' => $topic->id])}}"> {{$topic->title}}</a>
-            </h2>
-            <p>{!!nl2br($topic->intro)!!}</p>
-            @if ($mostRecentPostTime = $topic->most_recent_post_time)
-                <p class="small text-muted">Latest Post {{$mostRecentPostTime->diffForHumans()}}</p>
-            @endif
-        </div>
+            @include('topics._read', $topic)
         @endforeach
         @endif
 
