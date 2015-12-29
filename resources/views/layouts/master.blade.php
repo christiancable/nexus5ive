42570128a2c1f8c999c6bf7ca7a987a50412f6a5
Christian Cable <christiancable@gmail.com>
2015-12-29 11:58:53 +0000

    Layouts - pointing to https version of external stylesheet

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,8 +6,7 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/css/app.css" rel="stylesheet">
-    <link rel="stylesheet" href="http://bootswatch.com/cerulean/bootstrap.css">
-    {{-- <link rel="stylesheet" href="http://code.steadman.io/telestrap/telestrap/css/bootstrap-theme.css"> --}}
+    <link rel="stylesheet" href="https://bootswatch.com/cerulean/bootstrap.css">
 </head>
 <body>
 
