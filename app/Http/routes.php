700b68beaa6e80c82b17808130e027f69d56dfdb
Christian Cable <christiancable@gmail.com>
2015-12-30 19:52:03 +0000

    Registrations can now be turned on or off by setting NEXUS_ALLOW_REGISTRATIONS in .env

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,8 +12,11 @@
 */
 
 // authentication
-// Route::get('auth/register', 'Auth\AuthController@getRegister');
-// Route::post('auth/register', 'Auth\AuthController@postRegister');
+if (env('NEXUS_ALLOW_REGISTRATIONS') == true) {
+    Route::get('auth/register', 'Auth\AuthController@getRegister');
+    Route::post('auth/register', 'Auth\AuthController@postRegister');
+}
+
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
