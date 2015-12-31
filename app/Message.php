a51ec95d421b401dbc991ccf8ac6efd480b69e1f
Christian Cable <christiancable@gmail.com>
2015-12-31 15:28:33 +0000

    Added time to fillable to allow the message date to be set via a form

--- app/Message.php
+++ app/Message.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    protected $fillable = ['user_id','author_id','text','read'];
+    protected $fillable = ['user_id','author_id','text','read', 'time'];
 
     protected $casts = [
         'read' => 'boolean',
