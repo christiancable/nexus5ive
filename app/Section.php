6937ede794c0957562fe56d2d5ecb83c6cee15ef
Christian Cable <christiancable@gmail.com>
2016-01-04 19:54:56 +0000

    Sections - adding SoftDelete

--- app/Section.php
+++ app/Section.php
@@ -3,16 +3,41 @@
 namespace Nexus;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Log;
 
 class Section extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['id','title','intro','user_id','parent_id', 'weight'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        // Attach event handler for deleting a section
+        Section::deleting(function($section) {
+           
+            /*
+            to keep a cascading delete when using softDeletes we must remove the related models here
+             */
+            $children = ['sections', 'topics'];
+            Log::info("Deleting Section " . $section->id);
+            foreach ($children as $child) {
+                if ($section->$child()) {
+                        Log::info(" -   removing section->$child");
+                        $section->$child()->delete();
+                }
+            }
+        });
+    }
 
     // users
     
     public function moderator()
     {
-        return $this->hasOne('Nexus\User', 'id', 'user_id');
+        return $this->belongsTo('Nexus\User', 'user_id', 'id');
     }
 
     // sections
---
+++ database/migrations/2016_01_04_190045_add_soft_delete_to_sections_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSoftDeleteToSectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sections', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sections', function (Blueprint $table) {
+            $table->dropColumn('deleted_at');
+        });
+    }
+}
