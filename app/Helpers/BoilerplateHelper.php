0930e11f8736659437e05fff312c697faa16bd84
Christian Cable <christiancable@gmail.com>
2016-02-16 20:03:06 +0000

    Boilerplate - added new boilerplate helper class with formatting help method

---
+++ app/Helpers/BoilerplateHelper.php
@@ -0,0 +1,53 @@
+<?php
+namespace Nexus\Helpers;
+
+/**
+* a class to return standard chunks of text
+*
+**/
+
+class BoilerplateHelper
+{
+	public static function formattingHelp()
+	{
+
+		$helpText = <<<TEXT
+<strong>Basics</strong>
+<pre>
+some **bold** text
+</pre>
+<pre>
+some _italics_
+</pre>
+<hr/>
+<strong>Links</strong>
+<pre>
+Here is a link [click here](https://nexus5.org.uk).
+</pre>
+or just paste in the address and it will be clickable
+<hr/>
+<strong>Images</strong>
+<pre>
+![Look a picture](http://example.com/picture.jpg)
+</pre>
+or
+<pre>
+[picture-]http://example.com/picture.jpg[-picture]
+</pre>
+<hr/>
+<strong>Lists</strong>
+<pre>
+Star Treks:
+
+- Original Series
+- Next Generation
+- Deep Space Nine
+- Voyager
+- Enterprise
+</pre>
+TEXT;
+
+		return $helpText;
+
+	}
+}
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -29,46 +29,8 @@
             </div>
         </div>
 
-
-<?php
-$helpText = <<<TEXT
-<strong>Basics</strong>
-<pre>
-some **bold** text
-</pre>
-<pre>
-some _italics_
-</pre>
-<hr/>
-<strong>Links</strong>
-<pre>
-Here is a link [click here](https://nexus5.org.uk).
-</pre>
-or just paste in the address and it will be clickable
-<hr/>
-<strong>Images</strong>
-<pre>
-![Look a picture](http://example.com/picture.jpg)
-</pre>
-or
-<pre>
-[picture-]http://example.com/picture.jpg[-picture]
-</pre>
-<hr/>
-<strong>Lists</strong>
-<pre>
-Star Treks:
-
-- Original Series
-- Next Generation
-- Deep Space Nine
-- Voyager
-- Enterprise
-</pre>
-TEXT;
-?>
         <div class="col-md-10">
-            <p  class="pull-right" data-toggle="popover" data-html="true" title="Formating Help" data-placement="left" data-content="{!! $helpText !!}"><u>Formatting Help</u></p>
+            <p  class="pull-right" data-toggle="popover" data-html="true" title="Formating Help" data-placement="left" data-content="{!! Nexus\Helpers\BoilerplateHelper::formattingHelp() !!}"><u>Formatting Help</u></p>
         </div>
 
 </div>
