08ab59345c32b917883868e8bd679149db795c67
Christian Cable <christiancable@gmail.com>
2016-02-28 15:47:03 +0000

    Small screen improvements - usernames and times no longer get all squished on smaller screens

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,22 +7,29 @@
     @endif
 
     <div class="panel-body">
-
-        @if ($readProgress < $post->time)
-            <span class="pull-right text-info">{{ date('D, F jS Y - H:i', strtotime($post->time)) }}</span>
-        @else 
-            <span class="pull-right text-muted">{{ date('D, F jS Y - H:i', strtotime($post->time)) }}</span>   
-        @endif 
-
-        @if (isset($post->author))
-            <span><a href="{{ action('Nexus\UserController@show', ['username' => $post->author->username]) }}">{{$post->author->username}}</a> ({{$post->popname}})</span>
-        @else
-            <span>Unknown User (Unknown User)</span>
-        @endif
+        <div class="row">
+            <div class="col-sm-12 col-md-6">       
+            @if (isset($post->author))
+                <span><a href="{{ action('Nexus\UserController@show', ['username' => $post->author->username]) }}">{{$post->author->username}}</a> ({{$post->popname}})</span>
+            @else
+                <span>Unknown User (Unknown User)</span>
+            @endif
+            </div>
+            <div class="col-sm-12 col-md-6">
+            <?php
+                $postTime = $post->time;
+                $formattedTime = date('D, F jS Y - H:i', strtotime($post->time));
+            ?>  
+            @if ($readProgress < $postTime)
+                <span class="pull-right text-info visible-lg visible-md">{{ $formattedTime }}</span>
+                <span class="visible-sm visible-xs text-info">{{ $formattedTime }}</span>
+            @else 
+                <span class="pull-right text-muted visible-lg visible-md">{{ $formattedTime }}</span>   
+                <span class="visible-sm visible-xs text-muted">{{ $formattedTime }}</span>   
+            @endif     
+          </div>
+        </div>
         <hr>
         <p>{!! Nexus\Helpers\NxCodeHelper::nxDecode($post->text) !!}</p>
-
     </div>
-    {{-- <li>{{$post->update_user_id}}</li> --}}
 </div>
-    {{-- <li>{{$post->html}}</li> --}}
