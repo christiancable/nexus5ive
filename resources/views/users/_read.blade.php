f418d8b54eeeb4c497aeca4af04973018c402148
Christian Cable <christiancable@gmail.com>
2016-03-12 13:07:33 +0000

    User profile - allow formatting in user info section

--- resources/views/users/_read.blade.php
+++ resources/views/users/_read.blade.php
@@ -30,7 +30,7 @@
                 </dl>
                 </div>
 
-                <div class="well">{!! nl2br($user->about) !!}</div>
+                <div class="well">{!! Nexus\Helpers\NxCodeHelper::nxDecode($user->about) !!}</div>
                 @if (count($user->sections))
      {{--                <h2>Sections</h2> --}}
                     <span>If you like <strong>{{$user->username}}</strong> then check out these sections they moderate </span>
