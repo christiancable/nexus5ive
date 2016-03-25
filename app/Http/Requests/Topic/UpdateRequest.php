d30531ba38294784686fd3c19fa2424a6d1aaff4
Christian Cable <christiancable@gmail.com>
2016-03-25 20:07:26 +0000

    Topic Move - validation rules - checking for ownership of destination or admin

--- app/Http/Requests/Topic/UpdateRequest.php
+++ app/Http/Requests/Topic/UpdateRequest.php
@@ -8,6 +8,10 @@ class UpdateRequest extends Request
 {
     /**
      * Determine if the user is authorized to make this request.
+     * a topic can be updated by the section moderator or by an administrator
+     * 
+     * a topic can be moved to another section if the user moderators that section or 
+     * they are an administrator
      *
      * @return bool
      */
@@ -19,11 +23,25 @@ class UpdateRequest extends Request
             $return = false;
         }
 
+        // does the user moderate the section that this topic is currently in?
         $topic = \Nexus\Topic::findOrFail($this::input('id'));
         if ($topic->section->moderator->id == \Auth::id()) {
             $return = true;
         }
+        
+        // is the user moving the topic to a section they moderate?
+        try {
+            \Auth::user()->sections()->where('id',$this::input('section_id'))->firstOrFail();
+        } catch (\Exception $e) {
+            $return = false;
+            \Log::error('Topic Update - Attempt to move to unowned section '. $e);
+        }
 
+        // if the user is an admin then we assume they can do all
+        if (\Auth::user()->administrator) {
+            $return = true;
+        }
+        
         return $return;
     }
 
