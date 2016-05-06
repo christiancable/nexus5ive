57356d2a1852c85bcb69347f2e22ddaa82f0a20d
Christian Cable <christiancable@gmail.com>
2016-05-06 22:07:53 +0100

    Section headers now support formatting

--- resources/views/sections/_header_view.blade.php
+++ resources/views/sections/_header_view.blade.php
@@ -1,5 +1,5 @@
 <div class="content">
     <h1>{{$section->title}}</h1>
-    <p class="lead">{{$section->intro}}</p>
+    <span class="lead">{!! Nexus\Helpers\NxCodeHelper::nxDecode($section->intro) !!}</span>
     <p>Moderated by: <a href="{{ action('Nexus\UserController@show', ['username' => $section->moderator->username])}}">{{$section->moderator->username}}</a></p>
 </div>
