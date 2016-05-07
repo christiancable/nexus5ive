3fde9f5a4f6630fa805a6b6d6fa9cd8b533eaee4
Christian Cable <christiancable@gmail.com>
2016-05-07 12:23:15 +0100

    Testing - test for soft deleting a section

--- tests/intergration/models/SectionTest.php
+++ tests/intergration/models/SectionTest.php
@@ -4,20 +4,31 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Nexus\User;
+use Nexus\Section;
 
 class SectionTest extends TestCase
 {
     use DatabaseTransactions;
         
-    public function test_deleting_section_soft_deletes_section()
+    public function test_deleting_section_soft_deletes_section_and_only_that_one()
     {
-        // given
-        // when
-        // then
-    
-        $user = \factory(User::class,1)->create();
-        print_r($user->toArray()); 
-        $this->assertTrue(false);
+        // GIVEN we have a section
+        $section = factory(Section::class,1)->create();
+        
+        // AND some other sections
+        factory(Section::class,10)->create();
+        
+        $sectionCount = Section::all()->count();
+
+        // WHEN that particular section is deleted
+        $section->delete();
+        
+        // THEN number of sections goes down by one
+        $sectionCountAfterDeletion = Section::all()->count();
+        $this->assertEquals($sectionCountAfterDeletion, $sectionCount-1);        
+
+        // AND that particular section is soft deleted
+        $this->assertTrue($section->trashed());
     }
     
     /*
