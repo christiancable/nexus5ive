0af3841efc8aeab1452e6551126e9f896b20e996
Christian Cable <christiancable@gmail.com>
2016-05-07 10:41:32 +0100

    useless test

---
+++ tests/intergration/SectionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class SectionTest extends TestCase
+{    
+    public function test_deleting_section_soft_deletes_section()
+    {
+        // given
+        // when
+        // then
+        
+        $this->assertTrue(false);
+    }
+    
+    /*
+    test_deleting_section_soft_deletes_its_topics
+    test_deleting_section_soft_deletes_its_subsections
+
+    TopicTest
+    test_deleting_topic_soft_deletes_its_posts
+    
+    */
+}
