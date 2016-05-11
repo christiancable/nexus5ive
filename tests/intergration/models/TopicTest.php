45a2a6bccc33c974a9f2766b248f4a052d4625d1
Christian Cable <christiancable@gmail.com>
2016-05-11 14:45:16 +0100

    TopicTest - added test for MostRecentPostTime accessor

--- tests/intergration/models/TopicTest.php
+++ tests/intergration/models/TopicTest.php
@@ -8,8 +8,8 @@ use Nexus\Post;
 
 class TopicTest extends TestCase
 {
-	use DatabaseTransactions;
-	
+    use DatabaseTransactions;
+    
     public function test_deleting_topic_soft_deletes_its_posts()
     {
         // GIVEN we have a topic with post
@@ -31,11 +31,36 @@ class TopicTest extends TestCase
         $this->assertEquals(Topic::withTrashed()->count(), 1);
         $this->assertEquals(Post::withTrashed()->where('topic_id', $topic->id)->count(), 20);
     }
+
+    public function test_MostRecentPostTime_returns_time_of_latest_post()
+    {
+        $faker = \Faker\Factory::create();
+
+        // GIVEN we have a topic with posts
+        $topic = factory(Topic::class, 1)->create();
+
+        // posts from the last month but not today
+        factory(Post::class, 20)
+            ->create(
+                ['topic_id' => $topic->id,
+                'time' => $faker->dateTimeThisMonth('-1 days')]
+            );
+
+        // the most recent post being from today
+        $newPost = factory(Post::class, 1)
+            ->create(
+                ['topic_id' => $topic->id,
+                'time' => new \DateTime('now')]
+            );
+
+        // WHEN we look at look at the MostRecentPostTime
+        // THEN we have the date of the most recent post
+        $this->assertEquals($topic->most_recent_post_time, $newPost->time);
+    }
 }
  /*
     @todo
 
-    test that getMostRecentPostTimeAttribute returns the time of the most recent post
     test that mostRecentlyReadPostDate returns the most recently read post for that user
     test that unreadPosts returns the posts which are unread by that user
  */
