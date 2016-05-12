df447fde23ff7c98d04cb6b5bf4a999f438d741d
Christian Cable <christiancable@gmail.com>
2016-05-12 19:15:10 +0100

    https://trello.com/c/gmDyDaUd - added test for Topic->mostRecentlyReadPostDate method

--- tests/intergration/models/TopicTest.php
+++ tests/intergration/models/TopicTest.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Nexus\Topic;
 use Nexus\Post;
+use Nexus\User;
 
 class TopicTest extends TestCase
 {
@@ -57,10 +58,57 @@ class TopicTest extends TestCase
         // THEN we have the date of the most recent post
         $this->assertEquals($topic->most_recent_post_time, $newPost->time);
     }
+
+    public function test_mostRecentlyReadPostDate_returns_time_of_most_recently_read_post_time()
+    {
+        $faker = \Faker\Factory::create();
+
+        // GIVEN  we have a user
+        $user = factory(User::class, 1)->create();
+
+        // AND we have a topic with posts
+        $topic = factory(Topic::class, 1)->create();
+        factory(Post::class, 20)
+            ->create(
+                ['topic_id' => $topic->id,
+                'time' => $faker->dateTimeThisMonth('-2 days')]
+            );
+            
+        // AND the most recent post being from yesterday
+        $newPost = factory(Post::class, 1)
+            ->create(
+                ['topic_id' => $topic->id,
+                'time' => $faker->dateTimeThisMonth('-1 days')]
+            );
+          
+        // WHEN the user reads the topic
+        \Nexus\Helpers\ViewHelper::updateReadProgress($user, $topic);
+
+        // THEN the date of the most recent post in the topic matches
+        // the one most recently read by the user
+        $this->assertEquals(
+            $topic->most_recent_post_time,
+            $topic->mostRecentlyReadPostDate($user->id)
+        );
+
+        // WHEN a new post is added now
+        $anotherPost = factory(Post::class, 1)
+            ->create(
+                ['topic_id' => $topic->id,
+                'time' => new \DateTime('now')]
+            );
+
+        // THEN the date of the most recent post in the topic does not matches
+        // the one most recently read by the user
+        $this->assertNotEquals(
+            $topic->most_recent_post_time,
+            $topic->mostRecentlyReadPostDate($user->id)
+        );
+
+    }
 }
  /*
     @todo
 
-    test that mostRecentlyReadPostDate returns the most recently read post for that user
     test that unreadPosts returns the posts which are unread by that user
  */
