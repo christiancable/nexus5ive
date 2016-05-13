2ce05214b0c34305413af8c44bf10a800a1b0628
Christian Cable <christiancable@gmail.com>
2016-05-13 07:23:02 +0100

    Tests for ViewHelper to check status values of 'new_posts' and 'never_read'

--- tests/intergration/helpers/ViewHelperTest.php
+++ tests/intergration/helpers/ViewHelperTest.php
@@ -7,6 +7,10 @@ use Nexus\User;
 use Nexus\Topic;
 use Nexus\Post;
 
+/*
+ @todo: unsubscribe status - once we have an unsubscribe method
+
+*/
 class ViewHelperTest extends TestCase
 {
 	use DatabaseTransactions;
@@ -56,6 +60,97 @@ class ViewHelperTest extends TestCase
             $topic->most_recent_post_time,
             \Nexus\Helpers\ViewHelper::getReadProgress($user, $topic)
         );
+    }
 
+    public function  test_getTopicStatus_indicates_new_posts_for_a_topic_with_new_posts()
+    {
+        $faker = \Faker\Factory::create();
+
+        // GIVEN we have a user
+         $user = factory(User::class, 1)->create();
+        // AND we have a topic
+        // with posts
+        // AND we have a topic with posts
+        $topic = factory(Topic::class, 1)->create();
+        factory(Post::class, 20)
+            ->create(
+                ['topic_id' => $topic->id,
+                'time' => $faker->dateTimeThisMonth('-2 days')]
+            );
+            
+        // AND the user has read the topic
+        \Nexus\Helpers\ViewHelper::updateReadProgress($user, $topic);
+
+        // WHEN a new post is added
+        $anotherPost = factory(Post::class, 1)
+            ->create(
+                ['topic_id' => $topic->id,
+                'time' => new \DateTime('now')]
+            );
+
+        // THEN the topic appears to have new posts to the user
+        $topicStatus = \Nexus\Helpers\ViewHelper::getTopicStatus($user, $topic);
+
+        $this->assertTrue($topicStatus['new_posts']);
+    }
+
+    public function test_getTopicStatus_indicates_no_new_posts_for_a_topic_with_no_new_posts()
+    {
+        $faker = \Faker\Factory::create();
+
+        // GIVEN we have a user
+         $user = factory(User::class, 1)->create();
+        // AND we have a topic
+        // with posts
+        // AND we have a topic with posts
+        $topic = factory(Topic::class, 1)->create();
+        factory(Post::class, 20)
+            ->create(
+                ['topic_id' => $topic->id,
+                'time' => $faker->dateTimeThisMonth('-2 days')]
+            );
+            
+        // AND the user has read the topic
+        \Nexus\Helpers\ViewHelper::updateReadProgress($user, $topic);
+
+        // THEN the topic appears to have no new posts to the user
+        $topicStatus = \Nexus\Helpers\ViewHelper::getTopicStatus($user, $topic);
+
+        $this->assertFalse($topicStatus['new_posts']);
+    }
+
+    public function test_getTopicStatus_indicates_never_read_for_a_never_viewed_topic()
+    {
+        $faker = \Faker\Factory::create();
+
+        // GIVEN we have a user
+         $user = factory(User::class, 1)->create();
+        
+        // WHEN we add a topic
+        $topic = factory(Topic::class, 1)->create();
+        
+        // THEN the topic appears to have new to the user
+        $topicStatus = \Nexus\Helpers\ViewHelper::getTopicStatus($user, $topic);
+
+        $this->assertTrue($topicStatus['never_read']);
+    }
+
+    public function test_getTopicStatus_does_not_indicate_never_read_for_a_viewed_topic()
+    {
+        $faker = \Faker\Factory::create();
+
+        // GIVEN we have a user
+         $user = factory(User::class, 1)->create();
+        
+        // AND we add a topic
+        $topic = factory(Topic::class, 1)->create();
+        
+        // WHEN the user has read the topic
+        \Nexus\Helpers\ViewHelper::updateReadProgress($user, $topic);
+
+        // THEN the topic does not appear new to the user
+        $topicStatus = \Nexus\Helpers\ViewHelper::getTopicStatus($user, $topic);
+
+        $this->assertFalse($topicStatus['never_read']);
     }
 }
