4751b275ae4619416378ee90206bf11a571366da
Christian Cable <christiancable@gmail.com>
2016-05-24 22:36:55 +0100

    Restore view for section - work in progress

--- resources/views/restore/section.blade.php
+++ resources/views/restore/section.blade.php
@@ -3,9 +3,27 @@
         <div class="col-sm-6">
             <p><strong>{{$section->title}}</strong></p>
             <p>{{$section->intro}}</p>
+            <?php
+                $topics = $section->trashedTopics->pluck('title')->toArray();
+            ?>
+            @if(count($topics) != 0 ) 
+                <strong>Contains</strong> <small>{!! implode(', ', $topics) !!}</small>
+            @endif
+
             <p class="small text-muted">Removed: {{$section->deleted_at->diffForHumans()}}</p>
         </div>
         <div class="col-sm-6">
+
+        {!! Form::open(
+        array(
+            // 'route'     => ['restore.section', $post->id],
+            'route'     => ['restore.section', $section->id],
+            'class'     => 'form',
+            // 'method'    => 'PATCH',
+            // 'name'      => 'Section'.$section->id,
+            )
+        ) !!}
+
             <label> Restore to  {!! 
                                     Form::select("destination",
                                     $destinationSections->pluck('title','id')->toArray(),
@@ -18,8 +36,9 @@
                         'type'  => 'submit',
                         'class' => "btn pull-right btn-info", 
                         )
-                ) 
+                )
             !!}
+            {!! Form::close() !!}
         </div>
     </div>
 </div>
