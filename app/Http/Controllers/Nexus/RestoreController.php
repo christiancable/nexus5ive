e8162f87a4e9d7533ab75ee403252a169e72d985
Christian Cable <christiancable@gmail.com>
2016-05-24 22:34:00 +0100

    helper for restoring topics and sections which have been soft deleted

---
+++ app/Http/Controllers/Nexus/RestoreController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Nexus\Http\Controllers\Nexus;
+
+use Illuminate\Http\Request;
+
+use Nexus\Http\Requests;
+use Nexus\Http\Controllers\Controller;
+
+class RestoreController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // $trashedSections =  \Auth::user()->sections()->onlyTrashed()->orderBy('deleted_at', 'desc')->get();
+        $trashedSections = \Nexus\Section::onlyTrashed()
+            ->where('user_id', \Auth::user()->id)
+            ->with('trashedTopics')
+            ->orderBy('deleted_at', 'desc')
+            ->get();
+        // add trashed sections which are children of moderated sections which are not moderated by the user
+        $breadcrumbs = \Nexus\Helpers\BreadcrumbHelper::breadcumbForUtility('Restore');
+        $destinationSections = \Auth::user()->sections()->get();
+
+        return view('restore.index', compact('trashedSections', 'breadcrumbs', 'destinationSections'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function section(Request $request, $id)
+    {
+        $trashedSection = \Nexus\Section::onlyTrashed()->findOrFail($id);
+        $destinationSection = \Nexus\Section::findOrFail($request->destination);
+     
+        \Nexus\Helpers\RestoreHelper::restoreSectionToSection($trashedSection, $destinationSection);
+     
+        $redirect = action('Nexus\SectionController@show', ['id' => $trashedSection->id]);
+        return redirect($redirect);
+    }
+}
