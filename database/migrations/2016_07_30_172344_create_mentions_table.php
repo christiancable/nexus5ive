5acc7e70888a3427758adf1e5e5f83fbd36514cb
Christian Cable <christiancable@gmail.com>
2016-07-30 18:54:43 +0100

    migration for mentions table

---
+++ database/migrations/2016_07_30_172344_create_mentions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMentionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mentions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_id')->unsigned()->unique();
+            $table->integer('user_id')->unsigned()->unique();
+            $table->boolean('read')->default(false);
+
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('posts')->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('mentions');
+    }
+}
