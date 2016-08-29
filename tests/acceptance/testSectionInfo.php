5dd912bf1b1453a2bebe15fc3fbc16fe37092dc8
Christian Cable <christiancable@gmail.com>
2016-08-29 17:31:47 +0100

    added testSectionInfo for holding acceptance tests about section views

---
+++ tests/acceptance/testSectionInfo.php
@@ -0,0 +1,106 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Nexus\User;
+use Nexus\Topic;
+use Nexus\Post;
+use Nexus\Section;
+
+class testSectionInfo extends TestCase
+{
+    use DatabaseTransactions;
+    
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUserCanSeeLatestPostTopic()
+    {
+        /* GIVEN that we have
+
+        a user
+        a section
+        a subsction with a number of topics
+        no posts
+        */
+        $faker = \Faker\Factory::create();
+
+        $user = factory(User::class)->create();
+ 
+        $section = factory(Section::class)
+        ->create([
+            'parent_id' => null,
+            'user_id' => $user->id,
+            ]);
+
+        $subSection = factory(Section::class)
+        ->create([
+            'parent_id' => $section->id,
+            'user_id' => $user->id,
+            ]);
+
+
+        $topic1 = factory(Topic::class)
+        ->create([
+            'section_id' => $subSection->id,
+            ]);
+
+        $topic2 = factory(Topic::class)
+        ->create([
+            'section_id' => $subSection->id,
+            ]);
+
+        $topic3 = factory(Topic::class)
+        ->create([
+            'section_id' => $subSection->id,
+            ]);
+
+        // the user should not see the Latest Post notice for any section
+        $this->actingAs($user)
+            ->visit('/section/' . $section->id)
+            ->dontSee('Latest Post in');
+
+        /* WHEN
+         a post is added to topic1 
+        */
+
+        /* THEN we should see
+        a Latest Post notice for topic 1
+        */
+
+        $post = factory(Post::class)
+        ->create(
+            ['topic_id' => $topic1->id,
+            'user_id' => $user->id,
+            ]
+        );
+
+        $this->actingAs($user)
+            ->visit('/section/' . $section->id)
+            ->see("Latest Post in")
+            ->see($topic1->title);
+
+        /* THEN WHEN
+        a post is added to topic2 we should
+        see Latest Post notice
+        see $topic2->title
+        not see $topic1->title
+        */
+
+        $post = factory(Post::class)
+        ->create(
+            ['topic_id' => $topic2->id,
+            'user_id' => $user->id,
+            ]
+        );
+
+        $this->actingAs($user)
+            ->visit('/section/' . $section->id)
+            ->see("Latest Post in")
+            ->see($topic2->title)
+            ->dontSee($topic1->title);
+    }
+}
