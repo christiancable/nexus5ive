395a04388e116a787b7f9104611b089a71fc2977
Christian Cable <christiancable@gmail.com>
2016-10-29 16:43:37 +0100

    Fixes to the Halloween theme and a different front page quote

--- resources/views/special/halloween/_login.blade.php
+++ resources/views/special/halloween/_login.blade.php
@@ -1,16 +1,23 @@
 <?php
-  $today = \Carbon\Carbon::now();
-  $halloween = \Carbon\Carbon::create($today->year, 10, 31, 0);
-  $daysToHalloween = $today->diffInDays($halloween, false);
+    $today = \Carbon\Carbon::now();
+    $halloween = \Carbon\Carbon::create($today->year, 10, 31);
+    $isItTime = $halloween->isSameDay($today);
 ?>
-@if ($daysToHalloween == 0)
+@if ($isItTime)
     <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/W7-uC0LDllM?rel=0" frameborder="0" allowfullscreen></iframe>
 @else 
-    <p class='lead'>This is not a dream... not a dream.</p>
-
-    <p>We are using your brain's electrical system as a <em>receiver</em>.</p>
-
-    <p>We are unable to transmit through conscious neural interference. You are receiving this broadcast as a dream.</p>
-
-    <p>We are transmitting from the year <strong>two, zero, one, six</strong>. You are receiving this broadcast in order to alter the events you are seeing. Our technology has not developed a transmitter strong enough to reach your conscious state of awareness, but this is not a dream. You are seeing what is actually occurring for the purpose of causality violation.</p>
+    <code>
+    You will not be saved by <br/>
+    the Holy Ghost.
+    </code>
+    <br/><br/>
+    <code>
+    You will not be saved by<br/>
+    the god Plutonium.
+    </code>
+    <br/><br/>
+    <code>
+    In fact,<br/>
+    <strong>YOU WILL NOT BE SAVED!</strong>
+    </code>
 @endif 
--- resources/views/special/halloween/_loginHeading.blade.php
+++ resources/views/special/halloween/_loginHeading.blade.php
@@ -1,14 +1,21 @@
 <?php
   $today = \Carbon\Carbon::now();
   $halloween = \Carbon\Carbon::create($today->year, 10, 31, 0);
-  $daysToHalloween = $today->diffInDays($halloween, false);
+  $daysToHalloween = 1 + $today->diffInDays($halloween, false);
+  $isItTime = $halloween->isSameDay($today);
 ?>
-@if ($daysToHalloween > 0) 
-  <p class="lead">{{$daysToHalloween}} more days to Halloween, Halloween, Halloween... Silver Shamrock!</p>
-@else
-  @if ($daysToHalloween == 0)
+@if (true == $isItTime) 
     <p class="lead">Hurry up it's Halloween, Halloween, Halloween... Silver Shamrock!</p>
-  @else 
-    <p class="lead">Silver Shamrock!!</p>
-  @endif 
+@else     
+    @if ($daysToHalloween > 0)
+      <p class="lead">{{$daysToHalloween}} more 
+        @if ($daysToHalloween == 1)
+           day
+        @else
+          days 
+        @endif 
+      to Halloween, Halloween, Halloween... Silver Shamrock!</p>
+    @else
+        <p class="lead">Silver Shamrock!!</p>
+    @endif
 @endif
