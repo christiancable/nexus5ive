42fe03e94414bba87912a44b8f61a6ea5e8d0029
Christian Cable <christiancable@gmail.com>
2016-11-19 19:34:26 +0000

    updated database config to use different character set to allow emoji

--- config/database.php
+++ config/database.php
@@ -70,7 +70,7 @@ return [
         //     'collation' => 'utf8_unicode_ci',
         //     'prefix'    => '',
         //     'strict'    => false,
-        // ], 
+        // ],
 
         'mysql' => [
             'driver'    => 'mysql',
@@ -78,8 +78,8 @@ return [
             'database'  => env('DB_DATABASE', ''),
             'username'  => env('DB_USERNAME', ''),
             'password'  => env('DB_PASSWORD', ''),
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+            'charset'   => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
         ],
