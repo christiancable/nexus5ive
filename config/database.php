b9742cca988946a55b20a056e0a735e7703cbfb1
Christian Cable <christiancable@gmail.com>
2017-04-21 19:32:00 +0100

    Removed strict mode in database config due to issues with search

--- config/database.php
+++ config/database.php
@@ -81,7 +81,7 @@ return [
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
         ],
 
