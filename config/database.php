fd546b631eff66cf3d0b76206c93aa3b72b6b36f
Christian Cable <christiancable@gmail.com>
2017-04-22 09:57:27 +0100

    Changed db encoding to re-enable emoji support. I'd stomped on this setting during the 5.3 update.

--- config/database.php
+++ config/database.php
@@ -71,15 +71,15 @@ return [
         //     'strict'    => false,
         // ],
 
-         'mysql' => [
+        'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+            'charset'   => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => false,
             'engine' => null,
