a3ef3316da671130618f03ee13643977dadb8fa9
Christian Cable <christiancable@gmail.com>
2017-04-29 17:10:26 +0100

    Moving js into nexus.js from blade templates - wip

--- resources/assets/js/nexus.js
+++ resources/assets/js/nexus.js
@@ -4,8 +4,29 @@ function toggleChevron(e) {
         .find("i.indicator")
         .toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
 }
+
+
+function postPreview(tab) {
+    $.ajax({
+        type: 'POST',
+        url: '/api/nxcode',
+        data: {
+            'text': $('#postText').val(),
+            '_token': $('input[name=_token]').val()
+        },
+        dataType: 'JSON',
+        success: function (data) {
+            if ($('input[name=title]').val()) {
+                $('#preview-title').html($('input[name=title]').val());
+            }
+            $('#preview-view').html(data.text);
+        }
+    })
+}
+
 /* export functions we went to be global */
 window.toggleChevron = toggleChevron;
+window.postPreview = postPreview;
 
 /* event listeners */
 
--- resources/views/javascript/_postPreviewTabs.blade.php
+++ resources/views/javascript/_postPreviewTabs.blade.php
@@ -11,22 +11,4 @@ jQuery(document).ready(function ($) {
         })
     @endforeach   
 });
-
-function postPreview(tab) {
-	$.ajax({
-	            type: 'POST',
-	            url: '/api/nxcode',
-	            data: {
-	                'text': $('#postText').val(),
-	                '_token': $('input[name=_token]').val()
-	            },
-	            dataType: 'JSON',
-	            success: function (data) {
-	            	if ($('input[name=title]').val()) {
-		                $('#preview-title').html($('input[name=title]').val());
-	            	}
-	                $('#preview-view').html(data.text);
-	            }
-	})
-}
 </script>
