655a5e0312ba1a53bec8229aa80772a42382a266
Christian Cable <christiancable@gmail.com>
2017-10-05 21:32:46 +0100

    Updating laravel mix to 5.4

---
+++ webpack.mix.js
@@ -0,0 +1,20 @@
+const { mix } = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/assets/js/app.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css')
+
+   // keep as an extra file too for when we use themes
+   .sass('resources/assets/sass/extra.scss', 'public/css')
+   .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap','public/fonts/bootstrap');
+   
