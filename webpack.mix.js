ae9dda8b76f13f0fdd0b72c45466324801b3d40d
Christian Cable <christiancable@gmail.com>
2017-11-20 20:34:23 +0000

    Added terrible excelsior theme...

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,5 +17,6 @@ mix.js('resources/assets/js/app.js', 'public/js')
    // keep as an extra file too for when we use themes
    .sass('resources/assets/sass/extra.scss', 'public/css')
    .sass('resources/assets/sass/spooky.scss', 'public/css')
+   .sass('resources/assets/sass/excelsior.scss', 'public/css')
    .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap','public/fonts/bootstrap');
    
