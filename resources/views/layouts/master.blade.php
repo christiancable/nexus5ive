12771c67c1b718aee834f664b08ee4913a47a29e
Christian Cable <christiancable@gmail.com>
2017-11-20 19:20:46 +0000

    Added theme choice to the layout, removed the halloween theme

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,30 +1,35 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="UTF-8"> 
-  @yield('meta')
+  <head>
+    <meta charset="UTF-8"> 
+    @yield('meta')
 
-   <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @if (config('nexus.bootstrap_theme'))
-    <link rel="stylesheet" href="{{config('nexus.bootstrap_theme')}}">
-    <link href="/css/extra.css" rel="stylesheet">
-  @else
-    <link href="/css/app.css" rel="stylesheet">
-  @endif
-  @if (config('nexus.special_event') === 'halloween')
-    <link rel="stylesheet" href="/css/spooky.css">
-  @endif
-  <link rel="apple-touch-icon" href="/apple-touch.png">
-</head>
-<body>
+    @if (config('nexus.bootstrap_theme'))
+      <link rel="stylesheet" href="{{config('nexus.bootstrap_theme')}}">
+      <link href="/css/extra.css" rel="stylesheet">
+    @else
+
+      @if (Auth::check())
+          <link href="{{ Auth::User()->theme->path}}" rel="stylesheet">
+      @else
+          <link href="/css/app.css" rel="stylesheet">
+      @endif
+
+    @endif
+
+    <link rel="apple-touch-icon" href="/apple-touch.png">
+  </head>
+
+  <body>
 
   @if (Auth::check())
-  <span id="top-toolbar">
-    @include('_toolbar')
-  </span>
+    <span id="top-toolbar">
+      @include('_toolbar')
+    </span>
   @endif 
 
   @yield('breadcrumbs')
@@ -34,35 +39,39 @@
   @yield('content')
 
   @if (Auth::check())
-  <nav class="navbar navbar-default navbar-fixed-bottom visible-xs">
-    <div class="container">
-      <ul class="nav navbar-nav">
-       <li class="text-center col-xs-6">
-         <a {!! App\Helpers\GoogleAnalyticsHelper::onClickEvent('BottomNavigation', 'Catch-Up') !!}
-         href="{{ action('Nexus\SectionController@leap')}}">
-         <span class="glyphicon glyphicon-circle-arrow-right" aria-hidden="true" style="vertical-align:middle"></span> Next</a>
-       </li> 
-       <li class="text-center col-xs-6">
-         <a {!! App\Helpers\GoogleAnalyticsHelper::onClickEvent('BottomNavigation', 'Latest') !!}
-         href="{{ action('Nexus\SectionController@latest')}}">
-         <span class="glyphicon glyphicon-time" aria-hidden="true" style="vertical-align:middle"></span> Latest</a>
-       </li> 
-     </ul>
-   </div>
- </nav>
- @endif
+    <nav class="navbar navbar-default navbar-fixed-bottom visible-xs">
+      <div class="container">
+        <ul class="nav navbar-nav">
+          
+          <li class="text-center col-xs-6">
+            <a {!! App\Helpers\GoogleAnalyticsHelper::onClickEvent('BottomNavigation', 'Catch-Up') !!}
+              href="{{ action('Nexus\SectionController@leap')}}">
+              <span class="glyphicon glyphicon-circle-arrow-right" aria-hidden="true" style="vertical-align:middle"></span> Next
+            </a>
+          </li>
 
+          <li class="text-center col-xs-6">
+            <a {!! App\Helpers\GoogleAnalyticsHelper::onClickEvent('BottomNavigation', 'Latest') !!}
+              href="{{ action('Nexus\SectionController@latest')}}">
+              <span class="glyphicon glyphicon-time" aria-hidden="true" style="vertical-align:middle"></span> Latest
+            </a>
+          </li> 
 
- <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
- <script src="/js/app.js"></script>
- @yield('javascript')
- <!-- Include all compiled plugins (below), or include individual files as needed -->
- 
- @if (Auth::check())
- @include('javascript._toolbar')
- @endif
+        </ul>
+      </div>
+    </nav>
+  @endif
 
- @include('_googleanaytics')
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="/js/app.js"></script>
+  @yield('javascript')
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  
+  @if (Auth::check())
+  @include('javascript._toolbar')
+  @endif
 
-</body>
+  @include('_googleanaytics')
+
+  </body>
 </html>
