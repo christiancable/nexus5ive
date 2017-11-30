e9498b7c4b54c66dbf3be6d3b805756d77a65ad9
Christian Cable <christiancable@gmail.com>
2017-11-30 10:12:02 +0000

    Added messages to topic request

--- app/Http/Requests/Topic/UpdateRequest.php
+++ app/Http/Requests/Topic/UpdateRequest.php
@@ -49,7 +49,21 @@ class UpdateRequest extends FormRequest
         
         return $return;
     }
-
+    
+    
+    /**
+    *
+    * @return array the validation messages to be return to the user
+    **/
+    public function messages() 
+    {
+        return [
+            "form.*.title.required" => 'Title is required. Think of this as the subject to be discussed',
+            "form.*.intro.required" => 'Introduction is required. Give a brief introduction to your topic'
+        ];    
+    }
+    
+    
     /**
      * Get the validation rules that apply to the request.
      *
