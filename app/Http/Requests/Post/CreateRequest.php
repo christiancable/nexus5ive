9b37309b1db6832b1b193bef6bb1f019d7a378c8
Christian Cable <christiancable@gmail.com>
2017-12-30 19:12:03 +0000

    Added policy for post creation

--- app/Http/Requests/Post/CreateRequest.php
+++ app/Http/Requests/Post/CreateRequest.php
@@ -10,46 +10,11 @@ use App\Section;
 class CreateRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * user should be
-     * logged in
-     * user administrator | user moderator | topic NOT ready only
-     *
      * @return bool
      */
-
-    protected $errorBag = 'post';
-
     public function authorize()
     {
-
-        $return = false;
-        $topic = Topic::findOrFail($this::input('topic_id'));
-        $section =  Section::findOrFail($topic->section_id);
-
-        if (\Auth::check()) {
-            $authUser = \Auth::user();
-
-            // is the user an administrator
-            if ($authUser->administrator) {
-                $return = true;
-            }
-
-            // OR is the user the moderator
-            if ($authUser->id === $section->moderator->id) {
-                $return = true;
-            }
-
-            // OR is the topic NOT ready only
-            if (!$topic->readonly) {
-                $return = true;
-            }
-        } else {
-            $return = false;
-        }
-        
-        return $return;
+        return true;
     }
 
     /**
---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Post;
+use App\Topic;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function view(User $user, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create posts.
+     *
+     * @param  App\User  $user
+     * @param  App\Topic  $topic
+     * @return bool
+     */
+    public function create(User $user, Topic $topic)
+    {
+        // admins can always create new posts
+        if ($user->administrator) {
+            return true;
+        }
+    
+        // moderators can always create new posts in topics they moderate
+        if ($user->id === $topic->section->moderator->id) {
+            return true;
+        }
+    
+        // other users can only create posts of the topic is not ready only
+        if (!$topic->readonly) {
+            return true;
+        }
+        
+    }
+
+    /**
+     * Determine whether the user can update the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function update(User $user, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function delete(User $user, Post $post)
+    {
+        //
+    }
+}
