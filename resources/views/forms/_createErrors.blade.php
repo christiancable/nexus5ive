73b4201f117aa99235461284959f4c0a3c2727df
Christian Cable <christiancable@gmail.com>
2018-02-24 13:45:54 +0000

    simplified error display

--- resources/views/forms/_createErrors.blade.php
+++ resources/views/forms/_createErrors.blade.php
@@ -1,15 +1,11 @@
-@if (Session::get('form') == $formName)
-  @if ($errors->all())
-    @section('javascript')
-    @parent
-    @include('javascript._jqueryCreateErrors', [$formContainer])
-    @endsection
-    <div class="col-md-12 alert alert-danger" role="alert">
-      <ul>
-        @foreach($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-      </ul>
-    </div>
-  @endif
-@endif
+@section('javascript')
+@parent
+@include('javascript._jqueryCreateErrors', [$formContainer])
+@endsection
+<div class="col-md-12 alert alert-danger" role="alert">
+  <ul>
+    @foreach($errors as $error)
+    <li>{{ $error }}</li>
+    @endforeach
+  </ul>
+</div>
