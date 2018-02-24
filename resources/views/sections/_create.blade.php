9a7d03e6c2ed93c88c166e2da058498d932646cb
Christian Cable <christiancable@gmail.com>
2018-02-24 13:45:36 +0000

    simplified form variables

--- resources/views/sections/_create.blade.php
+++ resources/views/sections/_create.blade.php
@@ -5,22 +5,20 @@
         'class' => 'form'
     )) !!}
     <?php
-        $formName = 'sectionCreate';
         $submitLabel = 'Add Section';
         $submitIcon = 'glyphicon-plus-sign';
         $submitType = 'btn-primary';
     ?>
 
-    {!! Form::hidden("form[$formName][parent_id]", $section->id) !!}
-    {!! Form::hidden("form[$formName][user_id]", Auth::user()->id) !!}  
+    {!! Form::hidden("parent_id", $section->id) !!}
 
     <div class="form-group">    
-        {!! Form::label("form[$formName][title]",'Title', ['class' => 'hidden']) !!}
-        {!! Form::text("form[$formName][title]", null, ['class'=> 'form-control', 'placeholder' => 'Title'])!!}
+        {!! Form::label("title",'Title', ['class' => 'hidden']) !!}
+        {!! Form::text("title", null, ['class'=> 'form-control', 'placeholder' => 'Title'])!!}
     </div>
     <div class="form-group">
-        {!! Form::label("form[$formName][intro]",'Introduction', ['class' => 'hidden']) !!}
-        {!! Form::textarea("form[$formName][intro]", null, ['class'=> 'form-control', 'rows' => '3', 'placeholder' => 'Introduction'])!!}
+        {!! Form::label("intro",'Introduction', ['class' => 'hidden']) !!}
+        {!! Form::textarea("intro", null, ['class'=> 'form-control', 'rows' => '3', 'placeholder' => 'Introduction'])!!}
     </div>
     
     <div class="row form-inline">
@@ -32,4 +30,7 @@
    
 {!! Form::close() !!}
 </div>
- @include('forms._createErrors', ['errors' => $errors, 'formName' => $formName, 'formContainer' => 'newSectionPanel'])
+
+@if ($errors->sectionCreate->all())
+ @include('forms._createErrors', ['errors' => $errors->sectionCreate->all(), 'formContainer' => 'newSectionPanel'])
+@endif 
