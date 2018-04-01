e038155c1be34f0ab17f1c0ba7f6be29abf8e831
Christian Cable <christiancable@gmail.com>
2018-04-01 12:57:01 +0100

    Improved display of errors when editing section info

--- resources/views/sections/_edit.blade.php
+++ resources/views/sections/_edit.blade.php
@@ -1,19 +1,23 @@
 {{-- this is for moderators to edit sub sections --}}
 <div class="panel panel-primary">
     <div class="well">
-        <?php
-            $formName = 'section'.$subSection->id;
-        ?>
-        {!! Form::open(
-            array(
-                'route'     => ['section.update', $subSection->id],
-                'class'     => 'form',
-                'method'    => 'PATCH',
-                'name'      => $formName,
-                )
-            ) 
-        !!}
-        
+
+    <?php
+    $errorBag = 'sectionUpdate' . $subSection->id;
+    $formName = 'section'.$subSection->id;
+    $submitLabel = 'Save Changes';
+    $submitIcon = 'glyphicon-pencil';
+    $submitType = 'btn-info';
+    ?>
+
+    {!! Form::open(
+        array(
+        'route'     => ['section.update', $subSection->id],
+        'class'     => 'form',
+        'method'    => 'PATCH',
+        'name'      => $formName,
+    )) !!}
+
         {!! Form::hidden("form[$formName][id]", $subSection->id) !!}
 
         <div class="form-group">
@@ -23,46 +27,49 @@
         <div class="form-group">
             {!! Form::textarea("form[$formName][intro]", $subSection->intro, ['class'=> 'form-control']) !!}
         </div>
-        <?php
-            $submitLabel = 'Save Changes';
-            $submitIcon = 'glyphicon-pencil';
-            $submitType = 'btn-info';
-        ?>
 
         <div class="row form-inline">
+
             <div class="col-md-6 form-group">
-                <label>
-                    Parent Section {!! 
-                    Form::select("form[$formName][parent_id]",
+                <label> Parent Section 
+                {!! 
+                    Form::select(
+                        "form[$formName][parent_id]",
                         $destinations->pluck('title','id')->toArray(),
                         $subSection->parent->id,
-                        ['class' => 'form-control'])
-                    !!}
+                        ['class' => 'form-control']
+                    )
+                !!}
                 </label>
             </div>
 
             <div class="col-md-6 form-group">
-                <label>
-                    Display Order {!!
-                        Form::selectRange("form[$formName][weight]",
+                <label> Display Order
+                {!!
+                    Form::selectRange(
+                        "form[$formName][weight]",
                         0,
                         10,
                         $subSection->weight,
-                        ['class' => 'form-control'])
-                    !!} 
+                        ['class' => 'form-control']
+                    )
+                !!}
                 </label>
             </div>
+
         </div>
 
         <div class="row">
             <div class="col-md-6 form-group">
-                <label>
-                    Moderator {!!
-                        Form::select("form[$formName][user_id]",
-                            \App\User::all()->pluck('username', 'id')->toArray(),
-                            $subSection->moderator->id,                    
-                            ['class' => 'form-control'])
-                    !!} 
+                <label> Moderator 
+                {!!
+                    Form::select(
+                        "form[$formName][user_id]",
+                        \App\User::all()->pluck('username', 'id')->toArray(),
+                        $subSection->moderator->id,                    
+                        ['class' => 'form-control']
+                    )
+                !!}
                 </label>
             </div>
         </div>
@@ -70,33 +77,32 @@
         <div class="row">    
             <div class="col-sm-12">
                 <div class="form-group">          
-                {!! 
-                    Form::button("<span class='glyphicon glyphicon-pencil'></span>&nbsp;&nbsp;Save Changes",
+                {!!
+                    Form::button(
+                        "<span class='glyphicon glyphicon-pencil'></span>&nbsp;&nbsp;Save Changes",
                         array(
                             'type'  => 'submit',
                             'class' => "btn pull-right btn-info col-xs-12 col-sm-3", 
                             'value' => $formName
-                            )
-                        ) 
+                        )
+                    ) 
                 !!}
                 </div>
             </div>
         </div>
 
-        {!! Form::close() !!}
+    {!! Form::close() !!}
 
-
-    </div>
- @if (Session::get('form') == $formName)
-    @if ($errors->any())
-        <div class="row">
-        <div class="col-sm-12">
-            <p class="alert alert-danger">
-            You need to <strong>give your section a title</strong>. Otherwise; <em>chaos</em>.
-            </p>
-            </div>
+    @if ($errors->$errorBag->all())
+        <br/>
+        <div class="alert alert-danger" role="alert">
+            <ul>
+            @foreach($errors->$errorBag->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+            </ul>
         </div>
     @endif 
-@endif
-</div>
 
+    </div> <!-- well -->
+</div> <!-- panel panel-primary -->
--- resources/views/sections/_subsection_moderator.blade.php
+++ resources/views/sections/_subsection_moderator.blade.php
@@ -1,3 +1,7 @@
+<?php
+$errorBag = 'sectionUpdate' . $subSection->id;
+$showErrors = count($errors->$errorBag->all());
+?>
 <div class="col-md-12">
 
   <ul class="nav nav-tabs settings-menu" id="subSection{{$subSection->id}}" role="tablist"> 
@@ -7,13 +11,21 @@
         </a> 
         <ul class="dropdown-menu" aria-labelledby="subSectionTabDrop{{$subSection->id}}" id="subSectionTabDrop{{$subSection->id}}-contents"> 
             
+            @if ($showErrors)
+            <li class="">
+            @else 
             <li class="active">
+            @endif 
               <a href="#subSection-dropDown{{$subSection->id}}-view" role="tab" 
                 id="subSection-dropDown{{$subSection->id}}-view-tab" data-toggle="tab" aria-controls="subSection-dropDown{{$subSection->id}}-view" 
                 aria-expanded="false">View</a>
             </li>
 
+            @if ($showErrors)
+            <li class="active">
+            @else 
             <li class="">
+            @endif 
               <a href="#subSection-dropDown{{$subSection->id}}-edit" role="tab"
                 id="subSection-dropDown{{$subSection->id}}-edit-tab" data-toggle="tab" aria-controls="subSection-dropDown{{$subSection->id}}-edit"
                 aria-expanded="true">Edit</a>
@@ -26,6 +38,18 @@
   </ul>
 
   <div class="tab-content" id="subSectionTabsContent{{$subSection->id}}"> 
+  
+  @if ($showErrors)
+      <div class="tab-pane fade " role="tabpanel" id="subSection-dropDown{{$subSection->id}}-view" 
+        aria-labelledby="subSection-dropDown{{$subSection->id}}-view-tab">
+        @include('sections._read', $subSection)
+      </div>
+
+      <div class="tab-pane fade active in" role="tabpanel" id="subSection-dropDown{{$subSection->id}}-edit" 
+        aria-labelledby="subSection-dropDown{{$subSection->id}}-edit-tab"> 
+        @include('sections._edit', $subSection)
+      </div> 
+  @else 
       <div class="tab-pane fade active in" role="tabpanel" id="subSection-dropDown{{$subSection->id}}-view" 
         aria-labelledby="subSection-dropDown{{$subSection->id}}-view-tab">
         @include('sections._read', $subSection)
@@ -35,6 +59,8 @@
         aria-labelledby="subSection-dropDown{{$subSection->id}}-edit-tab"> 
         @include('sections._edit', $subSection)
       </div> 
+  @endif 
+
   </div> 
 
 </div>
