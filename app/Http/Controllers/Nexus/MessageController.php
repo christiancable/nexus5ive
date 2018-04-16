881ff2f1c1eeaf44ec232d64f315df603f09dc40
Christian Cable <christiancable@gmail.com>
2018-04-16 19:10:13 +0100

    removing unused methods on messagecontroller

--- app/Http/Controllers/Nexus/MessageController.php
+++ app/Http/Controllers/Nexus/MessageController.php
@@ -58,16 +58,6 @@ class MessageController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  Request $request
@@ -104,50 +94,5 @@ class MessageController extends Controller
         Message::create($input);
 
         return redirect(action('Nexus\MessageController@index'));
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  Request  $request
-     * @param  int  $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
