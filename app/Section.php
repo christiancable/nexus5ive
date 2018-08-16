d253b23a787560fea72047ac3aba2a61d7bf9c87
Christian Cable <christiancable@gmail.com>
2018-08-16 07:06:49 +0100

    slight refactor of getMostRecentPostAttribue in an effort to be more _clean_

--- app/Section.php
+++ app/Section.php
@@ -9,20 +9,20 @@ use Log;
 class Section extends Model
 {
     use SoftDeletes;
-
+    
     protected $fillable = ['id','title','intro','user_id','parent_id', 'weight'];
     protected $dates = ['deleted_at'];
-
+    
     public static function boot()
     {
         parent::boot();
-
+        
         // Attach event handler for deleting a section
         Section::deleting(function ($section) {
-           
+            
             /*
             to keep a cascading delete when using softDeletes we must remove the related models here
-             */
+            */
             $children = ['sections', 'topics'];
             Log::info("Deleting Section $section->title - $section->id");
             foreach ($children as $child) {
@@ -37,26 +37,26 @@ class Section extends Model
             }
         });
     }
-
+    
     // users
     
     public function moderator()
     {
         return $this->belongsTo(\App\User::class, 'user_id', 'id');
     }
-
+    
     // sections
-
+    
     public function parent()
     {
         return $this->belongsTo(\App\Section::class, 'parent_id', 'id');
     }
-
+    
     public function sections()
     {
         return $this->hasMany(\App\Section::class, 'parent_id', 'id')->orderBy('weight', 'asc');
     }
-
+    
     // topics
     
     public function topics()
@@ -64,39 +64,41 @@ class Section extends Model
         // return $this->hasMany('App\Topic', 'topic_id', 'id')->orderBy('topic_weight', 'asc');
         return $this->hasMany(\App\Topic::class)->orderBy('weight', 'asc');
     }
-
+    
     // counts - hopefully faster ...
     public function getTopicCountAttribute()
     {
         return Topic::select(\DB::raw('count(id) as count'))->where('section_id', $this->id)->value('count');
     }
-
+    
     public function getSectionCountAttribute()
     {
         return Section::select(\DB::raw('count(id) as count'))->where('parent_id', $this->id)->value('count');
     }
-
+    
     public function trashedTopics()
     {
         return $this->hasMany(\App\Topic::class)->onlyTrashed()->orderBy('weight', 'asc');
     }
-
-
+    
+    
     // posts
-
+    
     public function getMostRecentPostAttribute()
     {
-        $post = null;
-
         $topicIDs = Topic::select('id')->where('section_id', $this->id)->get()->toArray();
-        $postID = Post::select('id')->whereIn('topic_id', $topicIDs)->orderBy('id', 'desc')->get()->first();
-        
-        if ($postID) {
-            $post = Post::find($postID->id);
+        if (0 == count($topicIDs)) {
+            return null;
         }
-        return $post;
-    }
 
+        $postID = Post::select('id')->whereIn('topic_id', $topicIDs)->orderBy('id', 'desc')->get()->first();
+        if (!$postID) {
+            return null;
+        }
+        
+        return Post::find($postID->id);
+    }
+    
     public function slug()
     {
         return str_slug($this->title, '-');
