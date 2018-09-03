259f99a15cd18c6644c0572e6bc5433b91a3c8bc
Christian Cable <christiancable@gmail.com>
2018-09-03 19:05:01 +0100

    resetting some config to default 5.5

--- config/services.php
+++ config/services.php
@@ -1,5 +1,7 @@
 <?php
+
 return [
+
     /*
     |--------------------------------------------------------------------------
     | Third Party Services
@@ -11,21 +13,26 @@ return [
     | to have a conventional place to find your various credentials.
     |
     */
+
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
     ],
+
     'ses' => [
         'key' => env('SES_KEY'),
         'secret' => env('SES_SECRET'),
         'region' => 'us-east-1',
     ],
+
     'sparkpost' => [
         'secret' => env('SPARKPOST_SECRET'),
     ],
+
     'stripe' => [
         'model' => App\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
+
 ];
