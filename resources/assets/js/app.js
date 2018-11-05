d35797f2667896a72803f6797a9b7e9891b9a3cf
Christian Cable <christiancable@gmail.com>
2018-11-05 19:57:16 +0000

    webpack builds with vue components and bootstrap - many issues with css

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,16 +1,34 @@
 
 /**
  * First we will load all of this project's JavaScript dependencies which
- * include Vue and Vue Resource. This gives a great starting point for
+ * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
 require('./bootstrap');
 
-/**
-* custom nexus js
-*/
+window.Vue = require('vue');
+
 require('./nexus');
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
+Vue.component('userlist', require('./components/Userlist.vue'));
+Vue.component('post-compose', require('./components/PostCompose.vue'));
+Vue.component('post-preview', require('./components/PostPreview.vue'));
+Vue.component('rendered-text', require('./components/RenderedText.vue'));
+// const files = require.context('./', true, /\.vue$/i)
+
+// files.keys().map(key => {
+//     return Vue.component(_.last(key.split('/')).split('.')[0], files(key))
+// })
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -18,11 +36,7 @@ require('./nexus');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('userlist', require('./components/Userlist.vue'));
-Vue.component('post-compose', require('./components/PostCompose.vue'));
-Vue.component('post-preview', require('./components/PostPreview.vue'));
-Vue.component('rendered-text', require('./components/RenderedText.vue'));
-
 const app = new Vue({
     el: '#app'
 });
+
