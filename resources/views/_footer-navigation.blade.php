ad3d3565ec954523b671cab9fca883be5c9c865d
Christian Cable <christiancable@gmail.com>
2018-11-12 18:25:58 +0000

    replaced custom rules for padding and block to BS utility classes

--- resources/views/_footer-navigation.blade.php
+++ resources/views/_footer-navigation.blade.php
@@ -4,14 +4,14 @@
 
             <div class="text-center col">
                 <a {!! App\Helpers\GoogleAnalyticsHelper::onClickEvent('BottomNavigation', 'Catch-Up') !!}
-                href="{{ action('Nexus\SectionController@leap')}}" class="text-white">
+                href="{{ action('Nexus\SectionController@leap')}}" class="text-white d-block py-3">
                     <span class="oi oi-arrow-circle-right" aria-hidden="true" style="vertical-align:middle"></span> Next
                 </a>
             </div>
 
             <div class="text-center col">
                 <a {!! App\Helpers\GoogleAnalyticsHelper::onClickEvent('BottomNavigation', 'Latest') !!}
-                href="{{ action('Nexus\SectionController@latest')}}" class="text-white">
+                href="{{ action('Nexus\SectionController@latest')}}" class="text-white d-block py-3">
                     <span class="oi oi-clock" aria-hidden="true" style="vertical-align:middle"></span> Latest
                 </a>
             </div>
