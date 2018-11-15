482ead8936079011387e6fcf9868b242b23abb3e
Christian Cable <christiancable@gmail.com>
2018-11-15 20:11:37 +0000

    latest posts screen

--- resources/views/topics/unread.blade.php
+++ resources/views/topics/unread.blade.php
@@ -12,17 +12,13 @@
 <div class="container">
 
     <div class="content">
-        {{-- <h1>Continue the Conversation</h1>
-        <p class="lead">Here's what you've missed&hellip;</p> --}}
-        <h1>{{$heading}}</h1>
+        <h1 class="display-4">{{$heading}}</h1>
         <p class="lead">{{$lead}}</p>
-        
     </div>
 
     <hr>
 
     <div class="content">
-
         @if (count($topics))
             @foreach ($topics as $topic)
             @include('topics._latest', $topic)
