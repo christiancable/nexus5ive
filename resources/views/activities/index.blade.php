12ed6708587c019455e18401c3ec3a449fd739e8
Christian Cable <christiancable@gmail.com>
2018-11-17 09:11:44 +0000

    Upgrade Who is Online screen to BS4

--- resources/views/activities/index.blade.php
+++ resources/views/activities/index.blade.php
@@ -12,41 +12,37 @@
 
 <div class="container">
 
-
-    <div>
-        <h1 class="display-4">Who is Online</h1>
-        <p class="lead">Hell is other people</p>
-    </div>
-
+    @include('_heading', [$heading = 'Who is Online', $lead = 'Hell is other people'])
 
     <div>
         @if (count($activities))
-        <table class="table table-striped table-condensed">
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>Name</th>
-                    <th class="d-none d-sm-table-cell">Popname</th>
-                    <th>Current Action</th>
-                    <th>Last Active</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($activities as $activity)
-                @include('activities._read', $activity)
-                @endforeach
-            </tbody>
-        </table>
+            <table class="table table-striped table-borderless">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th class="d-none d-sm-table-cell">Popname</th>
+                        <th>Current Action</th>
+                        <th>Last Active</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($activities as $activity)
+                    @include('activities._read', $activity)
+                    @endforeach
+                </tbody>
+            </table>
         @else 
-        <div>
-            <div class="alert alert-warning" role="alert">Looks like there's no one else here. But *you* are here. How odd. (╯°□°）╯︵ ┻━┻</div>
-        </div>
+            <div>
+                <div class="alert alert-warning" role="alert">Looks like there's no one else here. But *you* are here. How odd. (╯°□°）╯︵ ┻━┻</div>
+            </div>
         @endif
     </div>
 
     <hr>
+
     <div>
-    <small class="text-info">Based on activity from the last {{$activityWindow->diffForHumans(null,true)}}.</small>
+        <small class="text-info">Based on activity from the last {{$activityWindow->diffForHumans(null,true)}}.</small>
     </div>
 
 </div>
