bdd71746f6895c0134125764ab505df869f69f8b
Christian Cable <christiancable@gmail.com>
2018-11-18 15:57:55 +0000

    Topic update for Bootstap 4

--- resources/views/topics/_read.blade.php
+++ resources/views/topics/_read.blade.php
@@ -1,50 +1,52 @@
 <?php
 $status = \App\Helpers\ViewHelper::getTopicStatus(Auth::user(), $topic);
+
+// display style
+
+$textClass = 'text-primary';
+$icon = 'oi oi-comment-square';
+
+if ($status['unsubscribed']) {
+    $textClass = 'text-muted';
+    $icon = 'oi oi-thumb-down'; 
+} elseif ($status['new_posts']) { 
+    $textClass = 'text-danger';
+    $icon = 'oi oi-fire';
+} elseif ($status['never_read']) {
+    $textClass = 'text-success';
+    $icon = 'oi oi oi-star';
+}
+
+
+if ($topic->most_recent_post) {
+    $authorLink = $topic->most_recent_post->author->present()->profileLink;
+    if ($topic->secret == true) {
+        $authorLink = '<strong>Anonymous</strong>';
+    }
+}
+
+$topicLink = action('Nexus\TopicController@show', ['topic_id' => $topic->id]);
+
 ?>
-<div class="well topic">
-    <div class="row">
-        <div class="col-sm-9">
-            <h2>
-                @if ($status['unsubscribed'])
-                    <?php $textClass = 'text-muted' ?>
-                    <a href="{{ action('Nexus\TopicController@show', ['topic_id' => $topic->id])}}" class="{{$textClass}}"> 
-                    <span class="glyphicon glyphicon-eye-close {{$textClass}}" aria-hidden="true"></span>
 
-                @elseif ($status['new_posts'])
-                    <?php $textClass = 'text-danger' ?>
-                    <a href="{{ action('Nexus\TopicController@show', ['topic_id' => $topic->id])}}" class="{{$textClass}}"> 
-                    <span class="glyphicon glyphicon-fire {{$textClass}}" aria-hidden="true"></span>
+<div class="card mb-3">
+  <div class="card-body">
 
-                @elseif ($status['never_read'])
-                    <?php $textClass = 'text-warning' ?>
-                    <a href="{{ action('Nexus\TopicController@show', ['topic_id' => $topic->id])}}" class="{{$textClass}}"> 
-                    <span class="glyphicon glyphicon-asterisk {{$textClass}}" aria-hidden="true"></span>
-                    
-                @else 
-                    <?php $textClass = '' ?>
-                    <a href="{{ action('Nexus\TopicController@show', ['topic_id' => $topic->id])}}" class="{{$textClass}}"> 
-                    <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
-                @endif  
-                {{$topic->title}}</a>
-            </h2>
-            <p class="break-long-words">{!! App\Helpers\NxCodeHelper::nxDecode($topic->intro) !!}</p>
-        </div>
+    <a href="{{$topicLink}}">
+        <h2 class="card-title"><span class="{{$textClass}} {{$icon}}"></span> {{$topic->title}}</h2>
+    </a>
+    <p class="card-text">{!! App\Helpers\NxCodeHelper::nxDecode($topic->intro) !!}</p>
 
-        <div class="col-sm-3">
-            @if ($topic->most_recent_post)
-                <hr class="visible-xs"/>
-                <p class="small text-muted">Latest Post 
-                @if($topic->most_recent_post->title)
-                    <em>&ldquo;{{$topic->most_recent_post->title}}&rdquo;</em> 
-                @endif 
-                by
-                @if($topic->secret == true)
-                      Anonymous,
-                @else 
-                    {!! $topic->most_recent_post->author->present()->profileLink !!}, 
-                @endif 
-                    {{$topic->most_recent_post->time->diffForHumans()}}</p>
-            @endif
-        </div>
-    </div>
+    @if ($topic->most_recent_post)
+        <footer class="d-flex flex-row-reverse">
+            <p class="small text-muted">Latest Post 
+            @if($topic->most_recent_post->title)
+                <em>&ldquo;{{$topic->most_recent_post->title}}&rdquo;</em> 
+            @endif 
+            by
+            {!!$authorLink!!} {{$topic->most_recent_post->time->diffForHumans()}}</p>
+        </footer>
+    @endif
+        
+  </div>
 </div>
--- resources/views/topics/_update.blade.php
+++ resources/views/topics/_update.blade.php
@@ -1,70 +1,97 @@
-<div class="well">
-    {!! 
-        Form::open(
-            array(
-            'route' => ['topic.update', $topic->id],
-            'class' => 'form',
-            'method' => 'PATCH'
-        )) 
-    !!}
-    <?php
-        $formName = 'topicUpdate' . $topic->id;
-        $submitLabel = 'Save Changes';
-        $submitIcon = 'glyphicon-pencil';
-        $submitType = 'btn-info';
+<?php
+$formName = 'topicUpdate' . $topic->id;
+$errorBag = 'topicUpdate' . $topic->id;
+?>
 
-    ?>
+{!! 
+    Form::open(
+        array(
+        'route' => ['topic.update', $topic->id],
+        'class' => 'form',
+        'method' => 'PATCH'
+    )) 
+!!}
     {!! Form::hidden($formName . '[id]', $topic->id) !!}
-
     {!! Form::hidden($formName . '[secret]', false) !!}  
     {!! Form::hidden($formName. '[readonly]', false) !!}
+
 
     <div class="form-group">    
         {!! Form::label("title",'Title', ['class' => 'hidden']) !!}
         {!! Form::text($formName . '[title]', $topic->title, ['class'=> 'form-control', 'placeholder' => 'Title'])!!}
     </div>
+
     <div class="form-group">
         {!! Form::label("intro",'Introduction', ['class' => 'hidden']) !!}
         {!! Form::textarea($formName . "[intro]", $topic->intro, ['class'=> 'form-control', 'rows' => '3', 'placeholder' => 'Introduction'])!!}
     </div>
-    
-    <div class="row form-inline">
 
-        <div class="col-md-2">
-            <div class="checkbox">
+
+    <div class="d-md-flex justify-content-md-between">
+
+        <fieldset>
+            <div class="form-check">
                 @if ($topic->secret)
-                    <label>{!! Form::checkbox($formName . "[secret]", true, true)!!} Anonymous</label>
+                    <input class="form-check-input" type="checkbox" value="1" id="{{$formName}}[secret]" name="{{$formName}}[secret]" checked>
                 @else
-                    <label>{!! Form::checkbox($formName . "[secret]")!!} Anonymous</label>
+                    <input class="form-check-input" type="checkbox" value="1" id="{{$formName}}[secret]" name="{{$formName}}[secret]">
                 @endif
+                <label class="form-check-label" for="{{$formName}}[secret]">Anonymous</label>
             </div>
-            <div class="checkbox">
+
+            <div class="form-check">
                 @if ($topic->readonly)
-                    <label>{!! Form::checkbox($formName . "[readonly]", true, true)!!} Read Only</label>
+                <input class="form-check-input" type="checkbox" value="1" id="{{$formName}}[readonly]" name="{{$formName}}[readonly]" checked>
                 @else
-                    <label>{!! Form::checkbox($formName . "[readonly]")!!} Read Only</label>
+                <input class="form-check-input" type="checkbox" value="1" id="{{$formName}}[readonly]" name="{{$formName}}[readonly]">
                 @endif 
+                <label class="form-check-label" for="{{$formName}}[readonly]">Read Only</label>    
             </div>
-        </div>
+        </fieldset>
 
-        <div class="col-md-7 form-group">     
         @if(isset($moderatedSections))
-            <label>Section {!! Form::select($formName . "[section_id]", $moderatedSections, $topic->section_id, ['class' => 'form-control'])!!} 
-            </label>
-        @endif
-        <label>Order {!! Form::selectRange($formName . "[weight]", 0, 10, $topic->weight, ['class' => 'form-control'])!!} </label>
+            <div class="form-group form-inline">
+                <label class="mr-sm-2" for="{{$formName}}[section_id]">Section</label>
+                    {!! Form::select($formName . "[section_id]", $moderatedSections, $topic->section_id, ['class' => 'form-control'])!!}
+                </select>
+            </div>
+        @endif 
+
+        <div class="form-group form-inline">
+            <label class="mr-sm-2" for="{{$formName}}[weight]">Order</label>
+                {!! Form::selectRange($formName . "[weight]", 0, 10, $topic->weight, ['class' => 'form-control'])!!}
+            </select>
         </div>
+    </div>
 
 
-        <div class="col-md-3">
-                {!! Form::button("<span class='glyphicon $submitIcon'></span>&nbsp;&nbsp;" . $submitLabel, array('type' => 'submit', 'class' => "btn pull-right  col-xs-12 $submitType")) !!}
-        </div>
-    {!! Form::close() !!}
-    </div>   
+    <div class="d-flex flex-row-reverse bd-highlight">    
+            <div class="form-group ml-2">          
+                {!! Form::button("<span class='oi oi-pencil mr-2'></span>Save Changes",
+                    array(
+                        'type'  => 'submit',
+                        'class' => "btn btn-success"
+                        )
+                ) !!}
+            </div>
+            {!! Form::close() !!}
 
-    <?php $errorBag = 'topicUpdate' . $topic->id ?>
+            <form action="{{action('Nexus\TopicController@destroy', ['id' => $topic->id])}}" method="POST">
+                <div class="form-group">
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
+                    {!! Form::button("<span class='oi oi-box mr-2'></span>Archive Topic",
+                        array(
+                            'type'  => 'submit',
+                            'class' => "btn btn-warning"
+                            )
+                    ) !!}
+                </div>
+            {!! Form::close() !!}
+    </div>
+
+
     @if ($errors->$errorBag->all())
-    <br/>
     <div class="alert alert-danger" role="alert">
         <ul>
             @foreach($errors->$errorBag->all() as $error)
@@ -73,4 +100,3 @@
         </ul>
     </div>
     @endif
-</div>
