9fa8fbe9d5eb56350a365de8eb1cf73914cfc43c
Christian Cable <christiancable@gmail.com>
2018-11-18 11:48:38 +0000

    Post Compose compent to BS 4

--- resources/assets/js/components/PostPreview.vue
+++ resources/assets/js/components/PostPreview.vue
@@ -1,15 +1,14 @@
 <template>
-<div class="panel panel-info">
-    <div class="panel-heading" v-if="post.title">
-        <h3 class="panel-title" id="preview-title">{{post.title}}</h3>
+    <div class="card mb-3" :id="post.id">
+        <div class="card-header bg-primary text-white" v-if="post.title">
+            <span class="card-title">{{post.title}}</span>
+        </div>
+        <div class="card-body">
+            <p class="card-text">
+                <rendered-text :text="post.text"></rendered-text>
+            </p>
+        </div>
     </div>
-
-    <div class="panel-body">
-        <p id="preview-view">
-            <rendered-text :text="post.text"></rendered-text>
-        </p>
-    </div>
-</div>
 </template>
 
 <script>
