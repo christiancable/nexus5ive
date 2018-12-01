57090a63e06460f657ee5d889b515cdc4e4fa8fb
Christian Cable <christiancable@gmail.com>
2018-12-01 20:19:41 +0000

    Sizing for bottom nav icons

--- resources/views/_footer-navigation.blade.php
+++ resources/views/_footer-navigation.blade.php
@@ -1,18 +1,18 @@
 <footer id="footer-navigation" class="container">
     <nav class="fixed-bottom d-lg-none bg-info">
-        <div class="row">
+        <div class="row no-gutters">
 
             <div class="text-center col">
                 <a {!! App\Helpers\GoogleAnalyticsHelper::onClickEvent('BottomNavigation', 'Catch-Up') !!}
                 href="{{ action('Nexus\SectionController@leap')}}" class="text-white d-block py-3">
-                    <span class="oi oi-arrow-circle-right" aria-hidden="true" style="vertical-align:middle"></span> Next
+                    <span class="h3 oi oi-arrow-circle-right" aria-hidden="true" style="vertical-align:middle"></span> Next
                 </a>
             </div>
 
             <div class="text-center col">
                 <a {!! App\Helpers\GoogleAnalyticsHelper::onClickEvent('BottomNavigation', 'Latest') !!}
                 href="{{ action('Nexus\SectionController@latest')}}" class="text-white d-block py-3">
-                    <span class="oi oi-clock" aria-hidden="true" style="vertical-align:middle"></span> Latest
+                    <span class="h3 oi oi-clock" aria-hidden="true" style="vertical-align:middle"></span> Latest
                 </a>
             </div>
 
