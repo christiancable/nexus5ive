1174021ff32657872a6f250a20d5920c67562c8f
Christian Cable <christiancable@gmail.com>
2018-12-11 19:30:28 +0000

    BS4 spacing for comments

--- resources/views/comments/_edit.blade.php
+++ resources/views/comments/_edit.blade.php
@@ -1,20 +1,20 @@
 <tr>
-    <td class="col-sm-1"><strong>
+    <td class="col-1"><strong>
        {!! $comment->author->present()->profileLink !!}
     </strong>
 </td>
-<td class="col-sm-10 break-long-words">    
+<td class="col break-long-words">    
     @if ($comment->read === true)
     {{$comment->text}}
     @else 
     <strong>{{$comment->text}}</strong>
     @endif 
 </td>
-<td class="col-sm-1">
+<td class="col-1">
     <form action="{{action('Nexus\CommentController@destroy', ['id' => $comment->id])}}" method="POST">
     {{ csrf_field() }}
     {{ method_field('DELETE') }}
-        <button class="btn btn-default btn-xs danger"><span class="danger glyphicon glyphicon-trash" aria-hidden="true"></span> Delete </button>
+        <button class="btn btn-danger"><span class="oi oi-trash aria-hidden="true"></span> Delete </button>
     </form>
 </td>
 </tr>
--- resources/views/comments/_read.blade.php
+++ resources/views/comments/_read.blade.php
@@ -1,9 +1,9 @@
 <tr>
-    <td class="col-sm-2"><strong>
+    <td class="col-2"><strong>
         {!! $comment->author->present()->profileLink !!}
     </strong>
 </td>
-<td class="col-sm-10 break-long-words">    
+<td class="col break-long-words">    
     {{$comment->text}}
 </td>
 </tr>
