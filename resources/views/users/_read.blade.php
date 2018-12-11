975a62cd7c56f514be0ec772af85ee56891c619a
Christian Cable <christiancable@gmail.com>
2018-12-11 19:30:55 +0000

    BS4 updates for viewing a user profile

--- resources/views/users/_read.blade.php
+++ resources/views/users/_read.blade.php
@@ -1,50 +1,49 @@
 
-                <div class="row">
-                <dl class="dl-horizontal col-md-6">        
-                    <dt>Name</dt><dd>{{$user->name}}</dd>
+<div class="row">
 
-                    @if ($user->private != true)
-                        <dt>Email</dt><dd><a href="mailto:{{$user->email}}">{{$user->email}}</a></dd>
-                    @else
-                        <dt>Email</dt><dd><em>Hidden</em></dd>
-                    @endif
+    <dl class="col">        
+    @if ($user->private != true)
+        <dt>Email</dt><dd><a href="mailto:{{$user->email}}">{{$user->email}}</a></dd>
+    @else
+        <dt>Email</dt><dd><em>Hidden</em></dd>
+    @endif
+        <dt>Location</dt><dd>{{$user->location}}</dd>
+        <dt>Favourite Film</dt><dd>{{$user->favouriteMovie}}</dd>
+        <dt>Favourite Band</dt><dd>{{$user->favouriteMusic}}</dd>
+    </dl>
 
-                    <dt>Popname</dt><dd>{{$user->popname}}</dd>
-                    @if ($user->latestLogin)
-                    	<dt>Latest Visit</dt><dd>{{$user->latestLogin->diffForHumans()}}</dd>
-                    @else
-                    	<dt>Latest Visit</dt><dd>Never</dd>
-                    @endif
-                    
-                </dl>
+    <dl class="col">        
+    @if ($user->latestLogin)
+        <dt>Latest Visit</dt><dd>{{$user->latestLogin->diffForHumans()}}</dd>
+    @else
+        <dt>Latest Visit</dt><dd>Never</dd>
+    @endif
+        <dt>Total Posts</dt><dd>{{$user->totalPosts}}</dd>
+        <dt>Total Visits</dt><dd>{{$user->totalVisits}}</dd>
+    </dl>
 
-                <dl class="dl-horizontal col-md-6">        
-                    <dt>Location</dt><dd>{{$user->location}}</dd>
-                    <dt>Favourite Film</dt><dd>{{$user->favouriteMovie}}</dd>
-                    <dt>Favourite Band</dt><dd>{{$user->favouriteMusic}}</dd>
+</div>
 
-                    <dt>Total Posts</dt><dd>{{$user->totalPosts}}</dd>
-                    <dt>Total Visits</dt><dd>{{$user->totalVisits}}</dd>
+<div class="card mb-3 bg-light">
+    <div class="card-body border border-light rounded">
+        <div class="card-text">
+        {!! App\Helpers\NxCodeHelper::nxDecode($user->about) !!}
+        </div>
+    </div>
+</div>
 
 
-                </dl>
-                </div>
-
-                <div class="well">{!! App\Helpers\NxCodeHelper::nxDecode($user->about) !!}</div>
-                @if (count($user->sections))
-     {{--                <h2>Sections</h2> --}}
-                    <span>If you like <strong>{{$user->username}}</strong> then check out these sections they moderate </span>
-                    <!-- Single button -->
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Choose Section <span class="caret"></span>
-                      </button>
-                      <ul class="dropdown-menu">
-                        @foreach ($user->sections as $section)
-                            <li><a href="{{ action('Nexus\SectionController@show', ['section_id' => $section->id]) }}">{{$section->title}}</a></li>
-                        @endforeach
-                      </ul>
-                    </div>
-
-                   <hr> 
-                @endif
+@if (count($user->sections))
+<span>If you like <strong>{{$user->username}}</strong> then check out these sections they moderate </span>
+<div class="dropdown d-inline">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="sectionMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Choose Section
+    </button>
+    <div class="dropdown-menu" aria-labelledby="sectionMenuButton">
+        @foreach ($user->sections as $section)
+        <a class="dropdown-item" href="{{ action('Nexus\SectionController@show', ['section_id' => $section->id]) }}">{{$section->title}}</a>
+        @endforeach        
+    </div>
+</div>
+<hr> 
+@endif
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,36 +9,43 @@
 @endsection
 
 @section('content')
-        <div class="container">
-            <div class="content">
-                <h1>{{$user->username}}</h1>
-                <hr>
+    <div class="container">
 
-                @if (Auth::user()->id == $user->id)
-                    @include('users._edit', $user)
-                @else
-                    @include('users._read', $user)
-                @endif
+        @if (Auth::user()->id == $user->id)
+            @include('shared._heading', [
+                $heading = $user->username
+            ])
+            @include('users._edit', $user)
+        @else
+            @include('shared._heading', [
+                $heading = $user->username,
+                $lead = $user->name,
+                $introduction = $user->popname
+            ])
+            @include('users._read', $user)
+        @endif
 
-                <h2 id="comments">Comments</h2>
-                @include('comments.create', $user)
-                @if (count($comments))
-                    <table class="table table-striped table-condensed">
-                    <tbody>           
-                        @foreach ($comments as $comment)
-                            @if (Auth::user()->id == $user->id)
-                                @include('comments._edit', $comment)
-                            @else 
-                                @include('comments._read', $comment)
-                            @endif 
-                        @endforeach
-                    </tbody>
-                    </table>
+        <h2 id="comments">Comments</h2>
+
+        @include('comments.create', $user)
+
+        @if (count($comments))
+            <table class="table table-striped">
+                <tbody>           
+                @foreach ($comments as $comment)
                     @if (Auth::user()->id == $user->id)
-                        @include('comments._clear', $user)
-                    @endif
-                    {{ $comments->links() }}
-                @endif
-            </div>
-        </div>
+                        @include('comments._edit', $comment)
+                    @else 
+                        @include('comments._read', $comment)
+                    @endif 
+                @endforeach
+                </tbody>
+            </table>
+            @if (Auth::user()->id == $user->id)
+                @include('comments._clear', $user)
+            @endif
+            {{ $comments->links() }}
+        @endif
+        
+    </div>
 @endsection
