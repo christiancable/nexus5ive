ce5ee07e06afbd0976ea9dcdac21acb22328a197
Christian Cable <christiancable@gmail.com>
2018-12-13 17:46:19 +0000

    Updating login screen for BS4

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,104 +6,75 @@
 
 @section('content')
 
-<div class="container">
+<div class="container my-3">
 
-  <div class="content">
-    <h1>{{config('nexus.name')}}</h1>
-@if (config('nexus.special_event') === 'halloween')
-   @include('special.halloween._loginHeading')
-@else
-  <p class="lead">when time becomes a loop</p>
-@endif 
-  </div>
-  <hr/>
+  @include('shared._heading', [
+    $heading = config('nexus.name'),
+    $lead = 'When time becomes a loop'
+  ])
 
+<div class="row">
+  <div class="col">
 
-  <div class="row">
-    <div class="col-md-6">
+    <form class="form" role="form" method="POST" action="{{ url('/login') }}">
+    {{ csrf_field() }}
+  
+    @if (count($errors) > 0)
+    <div class="alert alert-danger">
+      There were some problems signing into your account:
+      <ul>
+      @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+      </ul>
+    </div>
+    @endif
 
-     <form class="form" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
-
-     {{-- {!! Form::open(array('url' => '/auth/login', 'class' => 'form')) !!} --}}
-
-     @if (count($errors) > 0)
-
-     <div class="alert alert-danger">
-       There were some problems signing into your account:
-       <ul>
-         @foreach ($errors->all() as $error)
-         <li>{{ $error }}</li>
-         @endforeach
-       </ul>
-     </div>
-
-     @endif
-
-
-     <div class="form-group">
-       {!! Form::label('username', 'Username', ['class' => 'sr-only']) !!}
-       <div class="input-group">
-         <div class="input-group-addon">
-           <i class="glyphicon glyphicon-user"></i>
-         </div>
-         {!! Form::text('username', null, 
-         array('class'=>'form-control', 'placeholder'=>'Username')) !!}
-       </div>
-     </div>
-
-     <div class="form-group">
-       {!! Form::label('Password', 'password', ['class' => 'sr-only']) !!}
-       <div class="input-group">
-         <div class="input-group-addon">
-           <i class="glyphicon glyphicon-lock"></i>
-         </div>
-       {!! Form::password('password', 
-       array('class'=>'form-control', 'placeholder'=>'Password')) !!}
-       </div>
-     </div>
-
-     <div class="form-group">
-       {!! Form::submit('Sign In', array('class'=>'btn btn-primary col-xs-12')) !!}
-     </div>
-
-
-      <div class="row">
-     <div class="form-group">
-
-      <div class="checkbox col-xs-12">
-       <label>
-         {!! Form::checkbox('remember', 'remember') !!} Keep me signed in
-       </label>
-       </div>
-     </div>
-
+    <div class="form-group">
+      {!! Form::label('username', 'Username', ['class' => 'sr-only']) !!}
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text"><span class="oi oi-person"></span></div>
+        </div>
+        {!! Form::text('username', null, ['class'=>'form-control', 'placeholder'=>'Username']) !!}
       </div>
-     {!! Form::close() !!}
+    </div>
 
-     <div class="text-center">    
-       <p><a href="{{ url('/password/reset') }}">Forgot Your Password?</a></p>
-     </div> 
+    <div class="form-group">
+      {!! Form::label('Password', 'password', ['class' => 'sr-only']) !!}
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text"><span class="oi oi-key"></span></div>
+        </div>
+        {!! Form::password('password', ['class'=>'form-control', 'placeholder'=>'Password']) !!}
+      </div>
+    </div>
 
+    <div class="form-group">
+      {!! Form::submit('Log In', array('class'=>'btn btn-primary')) !!}
+    </div>
 
-   </div>
-
-   <div class="col-md-6">
-
-@if (config('nexus.special_event') === 'halloween')
-  @include('special.halloween._login')
-@else
-<p class="lead">There are those who believe that <strong>spodding</strong> here began out there, far across the network, with tribes of users who may have been the forefathers of the <em>Prestoneites</em>, or the <em>Facebookers</em>, or the <em>Twitters</em>.</p>
-
+    <div class="form-group">
+      <div class="form-check">
+      {!! Form::checkbox('remember', true, null, ['class' => 'form-check-input', 'id' => 'remember']) !!}
+      {!! Form::label('remember','Remember me', ['class' => 'form-check-label']) !!}
+      </div>
+    </div>
+    {!! Form::close() !!}
+  </div> <!-- .col-md -->
+  
+  <div class="col">
+    <p class="lead">There are those who believe that <strong>spodding</strong> here began out there, far across the network, with tribes of users who may have been the forefathers of the <em>Prestoneites</em>, or the <em>Facebookers</em>, or the <em>Twitters</em>.</p>
     <p>That they may have been the architects of the great forums, or the lost civilizations of <em>Monochrome</em> or <em>anonyMUD</em>. Some believe that there may yet be brothers of man who even now fight to survive somewhere beyond the screen&hellip;</p>
-@endif
-  </div>
+  </div> <!-- .col-md -->
+
 </div> <!-- .row -->
-<hr/>
 
+    
+
+<div class="text-center">    
+  <p><a href="{{ url('/password/reset') }}">Forgot Your Password?</a></p>
+</div> 
+      
 </div> <!-- .container -->
-
-</div>
-
-</div>
 @endsection
