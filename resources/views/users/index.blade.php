4dad47722e8061c1ec177e4b43277a5311e8d747
Christian Cable <christiancable@gmail.com>
2018-12-17 22:06:29 +0000

    Added v-cloak to userlist

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,7 +9,6 @@
 @endsection
 
 @section('content')
-        
 
 <div class="container">
     @include('shared._heading', [$heading = 'Users', $lead = '"I fight for the Users"'])    
@@ -17,31 +16,31 @@
 
 <div class="container" id="users-list">
 
-<div id="app" v-cloak>
-<userlist :users="{{json_encode($users)}}"></userlist>
-</div>
+    <div v-cloak>
+        <userlist :users="{{json_encode($users)}}"></userlist>
+    </div>
 
-<div class="replace-with-vue card-deck">
+    <div class="replace-with-vue card-deck v-cloak">
 
-    @foreach ($users as $user)        
-        @include('users._panel', $user)
+        @foreach ($users as $user)        
 
-         {{-- non-moderators get a card desk layout --}}
-        {{-- wrap sub-sections: 1 col for sm, 2 for md, 3 for lg --}}
-        <div class="w-100 d-sm-block d-md-none"></div>
+            @include('users._panel', $user)
 
-        @if ($loop->iteration % 2 === 0) 
-        <div class="w-100 d-none d-md-block d-lg-none"></div>
-        @endif 
+            {{-- non-moderators get a card desk layout --}}
+            {{-- wrap sub-sections: 1 col for sm, 2 for md, 3 for lg --}}
+            <div class="w-100 d-sm-block d-md-none"></div>
 
-        @if ($loop->iteration % 3 === 0)
-        <div class="w-100 d-none d-lg-block"></div>
-        @endif  
+            @if ($loop->iteration % 2 === 0) 
+            <div class="w-100 d-none d-md-block d-lg-none"></div>
+            @endif 
 
+            @if ($loop->iteration % 3 === 0)
+            <div class="w-100 d-none d-lg-block"></div>
+            @endif  
 
-    @endforeach
+        @endforeach
+        
+    </div>
     
-
-</div>        
 </div>
 @endsection
