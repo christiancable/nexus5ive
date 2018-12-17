7b99e03990988d95f7f105c02516ddd48383f29b
Christian Cable <christiancable@gmail.com>
2018-12-17 20:03:28 +0000

    Fix sucky col widths in chrome for user comments

--- resources/views/comments/_edit.blade.php
+++ resources/views/comments/_edit.blade.php
@@ -1,20 +1,19 @@
 <tr>
-    <td class="col-1"><strong>
-       {!! $comment->author->present()->profileLink !!}
-    </strong>
-</td>
-<td class="col break-long-words">    
-    @if ($comment->read === true)
-    {{$comment->text}}
-    @else 
-    <strong>{{$comment->text}}</strong>
-    @endif 
-</td>
-<td class="col-1">
-    <form action="{{action('Nexus\CommentController@destroy', ['id' => $comment->id])}}" method="POST">
-    {{ csrf_field() }}
-    {{ method_field('DELETE') }}
-        <button class="btn btn-danger"><span class="oi oi-trash aria-hidden="true"></span> Delete </button>
-    </form>
-</td>
+    <td>
+        <strong>{!! $comment->author->present()->profileLink !!}</strong>
+    </td>
+    <td class="break-long-words">    
+        @if ($comment->read === true)
+        {{$comment->text}}
+        @else 
+        <strong>{{$comment->text}}</strong>
+        @endif 
+    </td>
+    <td>
+        <form action="{{action('Nexus\CommentController@destroy', ['id' => $comment->id])}}" method="POST">
+        {{ csrf_field() }}
+        {{ method_field('DELETE') }}
+            <button class="btn btn-danger"><span class="oi oi-trash" aria-hidden="true"></span> Delete </button>
+        </form>
+    </td>
 </tr>
--- resources/views/comments/_read.blade.php
+++ resources/views/comments/_read.blade.php
@@ -1,9 +1,9 @@
 <tr>
-    <td class="col-2"><strong>
+    <td><strong>
         {!! $comment->author->present()->profileLink !!}
     </strong>
 </td>
-<td class="col break-long-words">    
+<td class="break-long-words">    
     {{$comment->text}}
 </td>
 </tr>
