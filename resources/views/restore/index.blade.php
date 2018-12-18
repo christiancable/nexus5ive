7fa522cbad2deda2cfe4d289ebebdc0649921d57
Christian Cable <christiancable@gmail.com>
2018-12-18 00:04:55 +0000

    First pass of updating the archive to BS4

--- resources/views/restore/index.blade.php
+++ resources/views/restore/index.blade.php
@@ -11,122 +11,67 @@
 @section('content')
 
 <div class="container">
-    <h1>Your Archive</h1>
 
-    <span class="lead">
-    "But you know what? It's never too late to get it back."
-    </span>
-</div>
-<hr>
+@include('shared._heading', [
+            $heading = 'Your Archive',
+            $lead = "But you know what? It's never too late to get it back.",
+            $introduction = "Topics and Sections which you archive will appear here until you restore them"
+])
+
 
 @if ($destinationSections->count() == 0)
-	<div class="container">
-		<div  class="alert alert-warning">
-		You cannot restore any sections or topics because you do not moderate any place to restore them to. Sorry!
-		</div>
-	</div>
+  <div  class="alert alert-warning">
+  You cannot restore any sections or topics because you do not moderate any place to restore them to. Sorry!
+  </div>
 @else 
 	
-<div id="accordions">
-	<div class="container">
-		<h2>Sections</h2>
-		<hr>
-	</div>
-	
+<h2>Archived Sections</h2>
+
+  <button class="btn btn-success col-12" type="button" data-toggle="collapse" data-target="#sections" aria-expanded="false" aria-controls="sections">
+    <span class="oi oi-chevron-right mr-2"></span>View Sections to Restore
+  </button>
+
+
+<div class="collapse" id="sections">
 	@if ($trashedSections->count() != 0)
-    	<div class="panel-group container" id="trashedSectionsAccordion" 
-    	    role="tablist" aria-multiselectable="true">
-            <div class="panel panel-default">
-                
-                <div class="panel-heading" role="tab" id="trashedSections">
-                  <h2 class="panel-title">
-                    <a class="collapsed" role="button" data-toggle="collapse" 
-                        data-parent="#trashedSectionsAccordion" 
-                        href="#trashedSectionsPanel" 
-                        aria-expanded="false" 
-                        aria-controls="trashedSectionsPanel">
-                        <i class="indicator glyphicon glyphicon-chevron-right"></i> Sections to Restore
-                    </a>
-                  </h2>
-                </div>
-                
-                <div id="trashedSectionsPanel" class="panel-collapse collapse" role="tabpanel" 
-                  aria-labelledby="trashedSections">
-                    <div class="panel-body">
-                    @foreach($trashedSections as $section)
-                        @include('restore.section', $section)
-                    @endforeach
-                  </div>
-                </div>
-              </div>
-            </div>
+    @foreach($trashedSections as $section)
+        @include('restore.section', $section)
+    @endforeach    
 	@else 
-	
-    	<div class="container">
-    		<div  class="alert alert-info">
-    			You don't have any archived sections to restore.
-    		</div>
-    	</div>
+  
+    <div  class="alert alert-info my-3">
+      You don't have any sections to restore.
+    </div>
+  
 	@endif 
-	
-
-    <div class="container">
-		<h2>Topics</h2>
-		<hr>
-	</div>
-	
-	@if ($trashedTopics->count() != 0)
-    	<div class="panel-group container" id="trashedTopicsAccordion" 
-    	    role="tablist" aria-multiselectable="true">
-            <div class="panel panel-default">
-                
-                <div class="panel-heading" role="tab" id="trashedTopics">
-                  <h2 class="panel-title">
-                    <a class="collapsed" role="button" data-toggle="collapse" 
-                        data-parent="#trashedTopicsAccordion" 
-                        href="#trashedTopicsPanel" 
-                        aria-expanded="false" 
-                        aria-controls="trashedTopicsPanel">
-                        <i class="indicator glyphicon glyphicon-chevron-right"></i> Topics to Restore
-                    </a>
-                  </h2>
-                </div>
-                
-                <div id="trashedTopicsPanel" class="panel-collapse collapse" role="tabpanel" 
-                  aria-labelledby="trashedTopics">
-                    <div class="panel-body">
-                    @foreach($trashedTopics as $topic)
-                        @include('restore.topic', $topic)
-                    @endforeach
-                  </div>
-                </div>
-              </div>
-            </div>
-	@else 
-	
-    	<div class="container">
-    		<div  class="alert alert-info">
-    			You don't have any archived topics to restore.
-    		</div>
-    	</div>
-	@endif 
-@endif          
-
 </div>
 
-@endsection
+<hr>
 
-@section('javascript')
-<script type="text/javascript">
+<h2>Archived Topics</h2>
 
-function toggleChevron(e) {
-    $(e.target)
-        .prev('.panel-heading')
-        .find("i.indicator")
-        .toggleClass('glyphicon-chevron-down glyphicon-chevron-right');
-}
+  <button class="btn btn-success col-12" type="button" data-toggle="collapse" data-target="#topics" aria-expanded="false" aria-controls="topics">
+    <span class="oi oi-chevron-right mr-2"></span>View Topics to Restore
+  </button>
 
-$('#accordions').on('hidden.bs.collapse', toggleChevron);
-$('#accordions').on('shown.bs.collapse', toggleChevron);
-</script>
+
+<div class="collapse" id="topics">
+	@if ($trashedTopics->count() != 0)
+
+      @foreach($trashedTopics as $topic)
+          @include('restore.topic', $topic)
+      @endforeach    
+
+	@else 
+  
+    <div  class="alert alert-info my-3">
+      You don't have any topics to restore.
+    </div>
+  
+	@endif 
+</div>
+
+@endif
+</div> 
+
 @endsection
--- resources/views/restore/section.blade.php
+++ resources/views/restore/section.blade.php
@@ -1,54 +1,43 @@
-<div class="row">
-
-    <div class="col-sm-12">
-        <h3>{{$section->title}}</h3>
-        <p class="small pull-right text-muted hidden-xs">Moderated by {{ $section->moderator->username}}</p>
-        <p>{{$section->intro}}</p>
-        <?php
-        $topics = $section->trashedTopics->pluck('title')->toArray();
-        ?>
+<?php
+    $topics = $section->trashedTopics->pluck('title')->toArray();
+?>
+<div class="card my-3">
+  <div class="card-body">
+    <h5 class="card-title">{{$section->title}}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Moderated by {{ $section->moderator->username}}</h6>
+    {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
         @if(count($topics) != 0 ) 
-            <p><strong>Topics &ndash;</strong> <small>{!! implode(', ', $topics) !!}</small></p>
+            <p class="card-text"><strong>Topics &ndash;</strong> <small>{!! implode(', ', $topics) !!}</small></p>
         @endif
-        <p class="small text-muted">Removed: {{$section->deleted_at->diffForHumans()}}</p>
-    </div>
+    
+    <p class="card-text small text-muted">Removed: {{$section->deleted_at->diffForHumans()}}</p>
 
-</div>
-
-@if ($destinationSections->count() != 0)
-    <div class="row">
-        <div class="col-sm-12">
-            {!! Form::open(
+    @if ($destinationSections->count() != 0)
+    {!! Form::open(
                 array(
                     'route'     => ['archive.section', $section->id],
                     'class'     => 'form',
                     )
-            ) !!}
-
-            <div class="row">
-
-                <div class="col-xs-12 col-sm-8">
-                    <label> Restore to  {!! 
-                        Form::select("destination",
-                            $destinationSections->pluck('title','id')->toArray(),
-                            ['class' => 'form-control'])
-                            !!}
-                    </label>
-                </div>
-
-                <div class=" col-xs-12 col-sm-4">
-                    {!!                  
-                        Form::button("<span class='glyphicon glyphicon-open'></span>&nbsp;&nbsp;Restore Section",
-                            array(
-                                'type'  => 'submit',
-                                'class' => "btn btn-primary col-xs-12 ", 
-                                )
-                            )
-                    !!}
-                </div>
+    ) !!}
+        <div class="form-row align-items-center justify-content-end">
+            <div class="col-auto my-1">
+                <label for="inlineFormCustomSelect">Restore section to</label>
             </div>
-            {!! Form::close() !!}
+            <div class="col-auto my-1">
+            {!! 
+                Form::select("destination",
+                    $destinationSections->pluck('title','id')->toArray(),
+                    null,
+                    ['class' => 'custom-select mr-sm-2']
+                )
+            !!}
+            </div>
+            
+            <div class="col-auto my-1">
+            <button type="submit" class="btn btn-primary">Restore Section</button>
+            </div>
         </div>
-    </div>
-@endif
-<hr>
+    </form>
+    @endif
+  </div>
+</div>
