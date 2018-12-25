f3149590722ee5f8101a51f677a874f9ed9f12e8
Christian Cable <christiancable@gmail.com>
2018-12-25 18:27:50 +0000

    removing legacy bootstrap 3 pagination support

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Pagination\Paginator; // BS3 pagination - laravel upgrade 5.6
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
