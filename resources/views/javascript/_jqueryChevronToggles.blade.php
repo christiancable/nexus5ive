0beb7ff1e23e518f39ae176b45025ed850b6d194
Christian Cable <christiancable@gmail.com>
2018-12-28 13:21:06 +0000

    removing unneeded js for disclosure toggles

--- resources/views/javascript/_jqueryChevronToggles.blade.php
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-    $('#newSectionAccordion').on('hidden.bs.collapse', toggleChevron);
-    $('#newSectionAccordion').on('shown.bs.collapse', toggleChevron);
-    $('#newTopicAccordion').on('hidden.bs.collapse', toggleChevron);
-    $('#newTopicAccordion').on('shown.bs.collapse', toggleChevron);   
-</script>
