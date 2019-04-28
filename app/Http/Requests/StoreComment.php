d436c14b080b6698c3dcfd26a2929710e335e230
Christian Cable <christiancable@gmail.com>
2019-04-28 17:53:20 +0100

    Comment validation returning to formrequest on laravel linter advice

---
+++ app/Http/Requests/StoreComment.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreComment extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'text' => 'required',
+            'user_id' => 'required|numeric|exists:users,id',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'text.required' => 'Comment Text required',
+            'user_id.required' => 'User ID required',
+            'user_id.exists' => 'Unknown user',
+        ];
+    }
+}
