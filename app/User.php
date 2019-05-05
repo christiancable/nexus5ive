e256aecedb4e48cb389dcf8ed8c0091021869eca
Christian Cable <christiancable@gmail.com>
2019-05-05 19:55:58 +0100

    nudge files to be more like default 5.7

--- app/User.php
+++ app/User.php
@@ -47,6 +47,15 @@ class User extends Authenticatable implements  MustVerifyEmail
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
     
     /* dates */
     
