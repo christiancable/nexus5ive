ac777c81a2ea10be9feb4a6ed5c01ac57c932ae1
Christian Cable <christiancable@gmail.com>
2019-06-16 18:00:01 +0100

    add scope for verified users - limits user list to that

---
+++ tests/Feature/UnverifiedUsersTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Carbon\Carbon;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class UnverifiedUsersTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function whenAUserIsVerifiedTheyAreAddedToTheVerifiedUserList()
+    {
+        $unverifiedUser = factory(User::class)->create([
+           'email_verified_at' => null
+        ]);
+        $userCount = User::verified()->get()->count();
+
+        $unverifiedUser->email_verified_at = Carbon::now();
+        $unverifiedUser->save();
+        $newUserCount = User::verified()->get()->count();
+
+        $this->assertEquals($userCount + 1, $newUserCount);
+    }
+
+    /**
+     * @test
+     */
+    public function unverifiedUsersDoNotAppearInVerifiedUserList()
+    {
+        $unverifiedUser = factory(User::class)->create([
+           'email_verified_at' => null
+        ]);
+
+        $allUsers = User::verified()->get();
+
+        $count = $allUsers->where('id', $unverifiedUser->id)->count();
+
+        $this->assertEquals($count, 0);
+    }
+}
