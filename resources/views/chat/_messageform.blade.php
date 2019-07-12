9509eaf7b36886de95a7f5955c72d59dd2d90193
Christian Cable <christiancable@gmail.com>
2019-07-12 16:55:17 +0100

    Mobile improvements for chat

--- resources/views/chat/_messageform.blade.php
+++ resources/views/chat/_messageform.blade.php
@@ -1,11 +1,12 @@
  <form method="POST" action="">
     @csrf
-    <div class="form-group">
-        <label class="sr-only" for="text">Message</label>
-        <textarea class="form-control" id="text" name="text" rows="3"></textarea>
+  <div class="form-row">
+    <div class="col-9 col-md-10">
+      <label class="sr-only" for="text">Message</label>
+      <textarea class="form-control" id="text" name="text" rows="3"></textarea>
     </div>
-    
-    <div class="form-group">
-        <input class="btn btn-primary" type="submit" value="Send">
+    <div class="col">
+       <input class="btn btn-primary" type="submit" value="Send">
     </div>
+  </div>
 </form>
