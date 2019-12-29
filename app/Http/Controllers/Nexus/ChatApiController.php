21242b9cce9943fb5828cfb329a6efa93eedc3bc
Christian Cable <christiancable@gmail.com>
2019-12-29 21:19:20 +0000

    avoid using laravel facade

--- app/Http/Controllers/Nexus/ChatApiController.php
+++ app/Http/Controllers/Nexus/ChatApiController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\Nexus;
 
-use Auth;
 use App\User;
 use App\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 
 class ChatApiController extends Controller
