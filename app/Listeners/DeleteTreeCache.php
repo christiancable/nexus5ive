42ad76c0483d9e3d011b525fe213a0792b3baff3
Christian Cable <christiancable@gmail.com>
2020-03-10 19:56:13 +0000

    migration for queues and redis throttling for rebuilding tree cache job

--- app/Listeners/DeleteTreeCache.php
+++ app/Listeners/DeleteTreeCache.php
@@ -4,10 +4,11 @@ namespace App\Listeners;
 
 use App\Tree;
 use App\Events\TreeCacheBecameDirty;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class DeleteTreeCache
+class DeleteTreeCache implements ShouldQueue
 {
     /**
      * Create the event listener.
@@ -27,6 +28,14 @@ class DeleteTreeCache
      */
     public function handle(TreeCacheBecameDirty $event)
     {
-        Tree::rebuild();
+        Redis::throttle('tree-cache-rebuild')->allow(1)->every(30)->then(function () {
+            // Job logic...
+            logger('QUEUE: adding TreeCacheBecameDirty');
+            Tree::rebuild();
+        }, function () {
+            // could not get lock, avoid adding to the queue
+            logger('QUEUE: avoiding adding TreeCacheBecameDirty');
+            return false;
+        });
     }
 }
