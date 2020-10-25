8c31d97c3392a32d9bb42531602f604daed8cdad
Christian Cable <christiancable@gmail.com>
2020-10-25 18:43:20 +0000

    removing old special event and theme settings in preference to the mode

--- config/nexus.php
+++ config/nexus.php
@@ -14,7 +14,6 @@ return [
     |
     */
     'admin_email' => env('NEXUS_ADMIN_EMAIL', ''),
-    'bootstrap_theme' => env('NEXUS_BOOTSTRAP_THEME'),
     'allow_registrations' => env('NEXUS_ALLOW_REGISTRATIONS', false),
     'recent_activity' => env('NEXUS_RECENT_ACTIVITY'),
     'name' => env('NEXUS_NAME'),
@@ -22,7 +21,6 @@ return [
     'recent_edit' => env('NEXUS_RECENT_EDIT'),
     'pagination' => env('NEXUS_PAGINATION', 10),
     'comment_pagination' => env('NEXUS_COMMENT_PAGINATION', 50),
-    'special_event' => env('NEXUS_SPECIAL_EVENT', ''),
     'notification_check_interval' => env('NEXUS_NOTIFICATION_CHECK_INTERVAL', 2000),
     'google_anaytics_activate' => env('GOOGLE_ANAYTICS_ACTIVATE', false),
     'google_analytics_id' => env('GOOGLE_ANALYTICS_ID'),
