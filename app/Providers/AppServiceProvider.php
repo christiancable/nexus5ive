efa90884b0d7254d2d68d1fc231d8353ce36f3ba
Christian Cable <christiancable@gmail.com>
2020-10-25 19:25:42 +0000

    stop query before we have db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,12 +17,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // share bbs mode to all views
+        // wrap in try because this isn't aways run when we have a db
         // @todo make this bit suck less
-        $seconds_to_cache = 3600;
-        $mode = Cache::remember('bbs_mode', $seconds_to_cache, function () {
-            return Mode::active()->with('theme')->first();
-        });
-
+        try {
+            $seconds_to_cache = 3600;
+            $mode = Cache::remember('bbs_mode', $seconds_to_cache, function () {
+                return Mode::active()->with('theme')->first();
+            });
+        } catch (\Throwable $th) {
+            $mode = null;
+        }
+        
         View::share('mode', $mode);
     }
 
