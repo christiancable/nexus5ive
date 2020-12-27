5a3f43f5bd3112c94ff861ffc7b13a6a69bed482
Christian Cable <christiancable@gmail.com>
2020-12-27 19:41:25 +0000

    WIP - nexus 2 file detection logic

---
+++ app/Nexus2/Helpers/Detect.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Nexus2\Helpers;
+
+/*
+
+
+A = article
+F = folder - link to another menu
+C = comment - ???
+M = message on the screen - just text
+R = run - runs a dos command
+I
+H 
+. = command - a command to the nexus parser
+
+^[#/;].*				{ BEGIN(0);		return IGNOREIT;		}
+[Aa][^ \t]*[ \t]*		{ BEGIN(NORMAL); return ARTICLETYPE;	}
+[Ff][^ \t]*[ \t]*		{ BEGIN(NORMAL); return FOLDERTYPE;		}
+[Cc][^ \t]*[ \t]*		{ BEGIN(NORMAL); return COMMENTTYPE;	}
+[Mm][^ \t]*[ \t]*		{ BEGIN(NORMAL); return MCOMMENTTYPE;	}
+[Rr][^ \t]*[ \t]*		{ BEGIN(NORMAL); return RUNTYPE;		}
+[Ii][^ \t]*[ \t]*		{ BEGIN(NORMAL); return INTERNALTYPE;	}
+[Hh][^ \t]*[ \t]*		{ BEGIN(NORMAL); return HEADERTYPE;		}
+"."						{ BEGIN(COMMAND);}
+
+
+
+*/
+class Detect
+{
+   /**
+    * isArticle
+    * is $file a nexus2 conversation file
+    * @return bool
+    */
+    public static function isArticle(string $file): bool
+    {
+        $header = substr($file, 0, 2);
+        if ("1b01" !== bin2hex($header)) {
+            return false;
+        }
+
+        if (strlen($file) === 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+   /**
+    * isMenu
+    * is $file a nexus2 menu?
+
+    * @param string $file
+    * @return void
+    */
+    public static function isMenu(string $file): bool
+    {
+        
+        // c 0 
+        $comment = "";
+
+        // m 0
+        $message = "";
+
+        // a lazy list of patterns for menu lines
+        $menuItems = [
+            // f 0 Y amstrad\amst.mnu * Amstrad section, especially for Amstrads!
+            'folder' => "/^f\s*\d*\s[a-z]\s\S*\s\*\s\S.*$/im",
+            
+            // a 100 100 U uses U Alternative @Uses for your machine!!
+            'article' => "/^a\s\d*\s\d*\s[a-z]\s\S*\s\S\s\S.*$/im",
+            
+            // h ***** SPLENDUDSVILLE ******
+            'heading' => "/^h\s*\S*.*$/im",
+
+            // . commands - this does not work???
+            'dot' => '/^\.\S*$/im',
+        ];
+
+        // .owner dummy vevaphon the_dud nightcrawler seventhson seeker
+        $command = "";
+
+        if (self::isArticle($file)) {
+            return false;
+        }
+
+        if (strlen($file) === 0) {
+            return false;
+        }
+
+        foreach ($menuItems as $type => $pattern) {
+            var_dump($type);
+            if (preg_match($pattern, $file)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
---
+++ tests/unit/nexus2/DetectTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Tests\Unit\Nexus2;
+
+use Tests\TestCase;
+use App\Nexus2\Helpers\Detect;
+
+class DetectTest extends TestCase
+{
+    protected $validArticle;
+    protected $validMenu;
+    
+    
+    public function loadFixtures()
+    {
+        $this->validArticle = file_get_contents("tests/Fixtures/Nexus2/Articles/DARK");
+        $this->validMenu = file_get_contents("tests/Fixtures/Nexus2/Menus/8BIT.MNU");
+    }
+
+    /**
+     * @test
+     * @group nx2
+     * @dataProvider potentialMenusAndExpectedResults
+     **/
+    public function isMenuDetectsMenus($input, $expectedResult)
+    {
+        $this->assertEquals(Detect::isMenu($input), $expectedResult);
+    }
+
+    /**
+     * @test
+     * @group nx2
+     * @dataProvider potentialFilesAndExpectedResults
+     **/
+    public function isArticleDetectsArticles($input, $expectedResult)
+    {
+        $this->assertEquals(Detect::isArticle($input), $expectedResult);
+    }
+
+    public function potentialMenusAndExpectedResults()
+    {
+        $this->loadFixtures();
+
+        return [
+            'blank string' => [
+                $input = '',
+                $expectedOutput = false,
+            ],
+            'article not a menu' => [
+                $input = $this->validArticle,
+                $expectedOutput = false,
+            ],
+            'valid menu' => [
+                $input = $this->validMenu,
+                $expectedOutput = true,
+            ],
+            'text' => [
+                $input = <<< TXT
+this is not an article or a menu, just some text
+TXT
+,
+                $expectedOutput = false,
+            ],
+        ];
+    }
+
+    public function potentialFilesAndExpectedResults()
+    {
+        $this->loadFixtures();
+        return [
+            'blank string' => [
+                $input = '',
+                $expectedOutput = false,
+            ],
+            'valid article' => [
+                $input = $this->validArticle,
+                $expectedOutput = true,
+            ],
+            'menu not a article' => [
+                $input = $this->validMenu,
+                $expectedOutput = false,
+            ],
+            'text' => [
+                $input = <<< TXT
+this is not an article or a menu, just some text
+TXT
+,
+                $expectedOutput = false,
+            ],
+        ];
+    }
+}
