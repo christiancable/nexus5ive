065da7ecca0fa4b2277dba2d32fcd0008acae245
Christian Cable <christiancable@gmail.com>
2021-04-28 17:56:43 +0100

    format nexus2 highlighting

---
+++ app/Nexus2/Helpers/Highlighter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Nexus2\Helpers;
+
+class Highlighter
+{
+    /**
+     * translates nexus2 formatting to markdown
+     * 
+     * @param string $raw - nexus2 text
+     * 
+     * @return string text with markdown formatting
+     * 
+     */
+    public function highlight(string $raw): string
+    {
+        $highlightedString = $this->curlyBraces($raw);
+        $highlightedString = $this->singleLetter($highlightedString);
+        return $highlightedString;
+    }
+
+
+    private function curlyBraces(string $raw): string
+    {
+        $re = '/{(.*)(}|$)/mU';
+        $subst = '**$1**';
+        $result = preg_replace($re, $subst, $raw);
+
+        return $result;
+    }
+
+    private function singleLetter(string $raw): string
+    {
+        $re = '/@(.)/mU';
+        $subst = '**$1**';
+        $result = preg_replace($re, $subst, $raw);
+
+        return $result;
+    }
+}
