b522c09a57dc1bb7c6215dbc96ca5061d78acbb3
Christian Cable <christiancable@gmail.com>
2022-01-03 18:42:06 +0000

    WIP - update test for nexus2 user class

--- tests/unit/nexus2/user.php
+++ tests/unit/nexus2/user.php
@@ -9,60 +9,70 @@ class User extends TestCase
 {
     /**
      * @test
-     * @group nx2
+     * @group        nx2
      * @dataProvider providerUserDataBasesAndUsers
      **/
     public function userIsHydratedFromUDB($udb, $expected)
     {
-        $user = new Nexus2User($udb);
-    
-        $this->assertEquals($user->username, $expected['Nick']);
-        $this->assertEquals($user->name, $expected['RealName']);
-        $this->assertEquals($user->popname, $expected['PopName']);        
-        $this->assertEquals($user->email, $expected['UserId'] . '@nexus2.imported');        
+        $user = new Nexus2User();
+        $user->hydrate($udb); 
+        $this->assertEquals($user->username(), $expected['Nick']);
+        $this->assertEquals($user->realName(), $expected['RealName']);
+        $this->assertEquals($user->popname(), $expected['PopName']);        
+        $this->assertEquals($user->userID(), $expected['UserId']);        
     }
 
     /**
      * @test
      */public function commentsIsAnArrayOfUserNamesAndMessages() 
     {
-        $handle = fopen("tests/Fixtures/Nexus2/Users/6/COMMENTS.TXT", "rb");
-        $contents = stream_get_contents($handle);
-        fclose($handle);
+        $user = new Nexus2User();
+        $comments = $user->parseComments(file_get_contents("tests/Fixtures/Nexus2/Users/6/COMMENTS.TXT"));
 
-        $comments = Nexus2User::parseComments($contents);
-    }
+        // expect that comments is an array and that each members of the array has a username and body
+        $this->assertIsArray($comments);
+        
+        // has count of 14 - see fixture file
+        $this->assertEquals(count($comments), 14);
 
-    public function providerUserDataBasesAndUsers()
-    {
-        $handle = fopen("tests/Fixtures/Nexus2/Users/6/NEXUS.UDB", "rb");
-        $contents = stream_get_contents($handle);
-        fclose($handle);
+        // check that each comment looks like a comment
+        foreach ($comments as $comment) {
+            $this->assertArrayHasKey('username', $comment);
+            $this->assertArrayHasKey('body', $comment);
+        }
 
-        return [
-            'fraggle' => [
-                $input = $contents,
-                $expectedOutput = [
-                    'Nick'          =>  'Fraggle',
-                    'UserId'        =>  'C.F.CABLE',
-                    'RealName'      =>  'Christian',
-                    'PopName'       =>  'totally tick tack',
-                    'Rights'        =>  '255',
-                    'NoOfEdits'     =>  '2053',
-                    'TotalTimeOn'   =>  '642446',
-                    'NoOfTimesOn'   =>  '65708',
-                    // 'Password'      =>  'fਇbm',
-                    'Dept'          =>  'angsthouse',
-                    'Faculty'       =>  'Part Time',
-                    'Created'       =>  'Tue 31/10/75 at 14:12:32',
-                    'LastOn'        =>  'Thu 3/6/99 at 17:27:05',
-                    // 'HistoryFile'   =>  'HISTORY.0',
-                    // 'BBSNo'         =>  '65542',
-                    // 'Flags'         =>  '45',
-                ],
+}
+
+public function providerUserDataBasesAndUsers()
+{
+    $handle = fopen("tests/Fixtures/Nexus2/Users/6/NEXUS.UDB", "rb");
+    $contents = stream_get_contents($handle);
+    fclose($handle);
+
+    return [
+        'fraggle' => [
+            $input = $contents,
+            $expectedOutput = [
+                'Nick'          =>  'Fraggle',
+                'UserId'        =>  'C.F.CABLE',
+                'RealName'      =>  'Christian',
+                'PopName'       =>  'totally tick tack',
+                'Rights'        =>  '255',
+                'NoOfEdits'     =>  '2053',
+                'TotalTimeOn'   =>  '642446',
+                'NoOfTimesOn'   =>  '65708',
+                // 'Password'      =>  'fਇbm',
+                'Dept'          =>  'angsthouse',
+                'Faculty'       =>  'Part Time',
+                'Created'       =>  'Tue 31/10/75 at 14:12:32',
+                'LastOn'        =>  'Thu 3/6/99 at 17:27:05',
+                // 'HistoryFile'   =>  'HISTORY.0',
+                // 'BBSNo'         =>  '65542',
+                // 'Flags'         =>  '45',
             ],
-        ];
-    }
+        ],
+    ];
+}
 
     // public function providerCommentsAndExpected()
     // {
