3625bce0cf7b0bca159e348f2c3329c3a923c540
Christian Cable <christiancable@gmail.com>
2022-01-03 16:18:14 +0000

    added article class and artisan command for viewing articles of comments

---
+++ app/Nexus2/Models/Article.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ * Article
+ *
+ * @author Christian Cable <christiancable@gmail.com>
+ * 
+ * a nexus2 article is a plain text file which may contain comments from users
+ * at a particular date and time
+ */
+namespace App\Nexus2\Models;
+
+class Article
+{
+    const DATE_LINE_START       = "\e\x01";
+    const USER_LINE_START       = "\e\x02";
+    const SUBJECT_LINE_START    = "\e\x03";
+    
+    private $_comments = [];
+    private $_content = '';
+    private $_path = null;
+    private $_preamble = '';
+
+    function __construct(string $filename = null)
+    {
+
+        $this->_path = $filename;
+        if (null != $this->_path) {
+            $this->parseFromFile($this->_path);
+        }
+    }
+
+
+    public function parse(string $string): void
+    {
+        $this->_content = $string;
+        $rawPosts = explode(self::DATE_LINE_START, $this->_content);
+
+        foreach ($rawPosts as $rawPost) {
+            if ($rawPost != '') {
+                if ($this->isComment($rawPost)) {
+                    $this->_comments[] = $this->parsePost($rawPost);
+                } else {
+                    $this->_preamble .= $rawPost;
+                }
+            }
+        }
+    }
+    
+    /**
+     * Does the string appear to be a comment? A very lazy check
+     *
+     * @param string $rawPost - a string which might
+     *                        be a nexus2 comment
+     * 
+     * @return bool
+     */
+    private function isComment(string $rawPost): bool
+    {
+        $lines = preg_split('/\R/', $rawPost);
+        return  strstr($lines[1], self::USER_LINE_START);
+    }
+    
+    /**
+     * Parses the file into an article
+     *
+     * @param string $filename - path to a file
+     * 
+     * @return void
+     */
+    private function parseFromFile(string $filename): void
+    {
+        $this->parse(file_get_contents($filename));
+    }
+        
+    /**
+     * Parses a string into a username and popname
+     *
+     * @param string $userline - string to parse
+     * 
+     * @return array username and popname
+     */
+    public function parseUserandPopname(string $userline): array
+    {
+        $pattern = '/' . self::USER_LINE_START .
+            '(?<popname>.*)(\) )(?<username>.*)$/';
+        preg_match_all($pattern, $userline, $matches, PREG_SET_ORDER, 0);
+        
+        // do we have the expected matches??
+        // dd($matches[0]);
+        return [
+            'username'=> $matches[0]['username'],
+            'popname' => $matches[0]['popname'],
+        ];
+    }
+
+    public function parsePost($rawPost)
+    {
+        /* post format, note subject line is optional
+        \e\x01 date
+        \e\x02 popname) username
+        \e\x03 subject
+        body
+        */
+        $post = [
+            'date'      => '',
+            'username'  => '',
+            'popname'   => '',
+            'subject'   => '',
+            'body'      => '',
+        ];
+
+        $lines = preg_split('/\R/', $rawPost);
+
+        $post['date'] = trim(ltrim($lines[0], self::DATE_LINE_START));
+        unset($lines[0]);
+        
+        if (strstr($lines[1], self::USER_LINE_START)) {
+            $userandpopname = $this->parseUserandPopname($lines[1]);
+            $post['username'] = $userandpopname['username'];
+            $post['popname'] = $userandpopname['popname'];
+            unset($lines[1]);
+        }
+        
+        if (strstr($lines[2], self::SUBJECT_LINE_START)) {
+            $post['subject'] = trim(ltrim($lines[2], self::SUBJECT_LINE_START));
+            unset($lines[2]);
+        }
+
+        $post['body'] = trim(implode("\n", $lines));
+
+        return $post;
+    }
+
+    /**
+     * Getter for comments
+     *
+     * @return array of Comment arrays
+     */
+    public function comments()
+    {
+        return $this->_comments;
+    }
+    
+    /**
+     * Getter for the first comment if it exists
+     *
+     * @return array - first comment in file
+     */
+    public function first(): ?array
+    {
+        return $this->_comments[0] ?? null;
+    }
+    
+    /**
+     * Getter for date of earliest comment if it exists
+     *
+     * @return string
+     */
+    public function date(): ?string
+    {
+        return $this->first()['date'] ?? null;
+    }
+    
+    /**
+     * Getter for article text which is not part of a comment
+     * often used for preamble to text files
+     *
+     * @return string
+     */
+    public function preamble(): string
+    {
+        return $this->_preamble;
+    }
+    
+    /**
+     * Getter for the filename of the article if known
+     *
+     * @return string
+     */
+    public function path(): ?string
+    {
+        return $this->_path;
+    }
+}
