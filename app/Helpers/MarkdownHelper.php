6a33a74f7adae6f2e4f596fe482a7029105894e4
Christian Cable <christiancable@gmail.com>
2022-10-24 17:59:35 +0100

    config the markdown and remove unwanted end of line character

--- app/Helpers/MarkdownHelper.php
+++ app/Helpers/MarkdownHelper.php
@@ -16,8 +16,27 @@ class MarkdownHelper
     public static function markdown($markdown)
     {
 
-        $html = Str::of($markdown)->markdown();
-
+        $config = [
+            'renderer' => [
+                'block_separator' => "\n",
+                'inner_separator' => "\n",
+                'soft_break'      => "\n",
+            ],
+            'commonmark' => [
+                'enable_em' => true,
+                'enable_strong' => true,
+                'use_asterisk' => true,
+                'use_underscore' => true,
+                'unordered_list_markers' => ['-', '*', '+'],
+            ],
+            'html_input' => 'escape',
+            'allow_unsafe_links' => false,
+            'max_nesting_level' => PHP_INT_MAX,
+            'slug_normalizer' => [
+                'max_length' => 255,
+            ],
+        ];
+        $html = Str::of($markdown)->markdown($config)->rtrim("\n");
         return $html;
     }
 }
