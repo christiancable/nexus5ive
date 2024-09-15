e1b210934d2889c480ab921b1a06071dc24c9aea
Christian Cable <christiancable@gmail.com>
2024-09-15 17:54:24 +0200

    Add themes

--- resources/views/nexus/_theme.blade.php
+++ resources/views/nexus/_theme.blade.php
@@ -1,6 +1,6 @@
 @if ($theme->external)
     <link href="{{ $theme->path }}" rel="stylesheet">
-    @vite(['resources/css/extra.css'])
+    @vite(['resources/css/extra.sass'])
 @else
     <link href="{{ asset($theme->path) }}" rel="stylesheet">
 @endif
