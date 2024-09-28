8a4137e8e86e63d75f9bce6c09477b714e22310c
Christian Cable <christiancable@gmail.com>
2024-09-28 10:00:17 +0100

    use size classes for the hero icons

--- resources/views/components/topic-heading.blade.php
+++ resources/views/components/topic-heading.blade.php
@@ -2,19 +2,19 @@
     <h2 class="card-title">
         @switch($icon)
             @case('unsubscribed')
-                <x-heroicon-s-hand-thumb-down class="icon_topic {{ $textClass }} " />
+                <x-heroicon-s-hand-thumb-down class="icon_large {{ $textClass }} " />
             @break
 
             @case('new_posts')
-                <x-heroicon-s-fire class="icon_topic {{ $textClass }}" />
+                <x-heroicon-s-fire class="icon_large {{ $textClass }}" />
             @break
 
             @case('never_read')
-                <x-heroicon-s-star class="icon_topic {{ $textClass }}" />
+                <x-heroicon-s-star class="icon_large {{ $textClass }}" />
             @break
 
             @default
-                <x-heroicon-s-chat-bubble-bottom-center-text class="icon_topic {{ $textClass }}" />
+                <x-heroicon-s-chat-bubble-bottom-center-text class="icon_large {{ $textClass }}" />
         @endswitch
         {{ $title }}
     </h2>
