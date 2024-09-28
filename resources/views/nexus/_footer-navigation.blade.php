477487fd0ee0e6fe147e5d507a36f8e55f0409e3
Christian Cable <christiancable@gmail.com>
2024-09-28 10:01:19 +0100

    use hero icons for the mobile bottom nav

--- resources/views/nexus/_footer-navigation.blade.php
+++ resources/views/nexus/_footer-navigation.blade.php
@@ -3,20 +3,19 @@
         <div class="row no-gutters">
 
             <div class="text-center col">
-                <a {!! App\Helpers\GoogleAnalyticsHelper::onClickEvent('BottomNavigation', 'Catch-Up') !!}
-                href="{{ action('App\Http\Controllers\Nexus\SectionController@leap')}}" class="text-white d-block py-3">
-                    <span class="h3 oi oi-arrow-circle-right mr-1" aria-hidden="true" style="vertical-align:middle"></span> Next
+                <a {!! App\Helpers\GoogleAnalyticsHelper::onClickEvent('BottomNavigation', 'Catch-Up') !!} href="{{ action('App\Http\Controllers\Nexus\SectionController@leap') }}"
+                    class="text-white d-flex align-items-center justify-content-center py-3">
+                    <x-heroicon-s-arrow-right-circle class="icon_medium mr-1" aria-hidden="true" />Next
                 </a>
             </div>
 
             <div class="text-center col">
-                <a {!! App\Helpers\GoogleAnalyticsHelper::onClickEvent('BottomNavigation', 'Latest') !!}
-                href="{{ action('App\Http\Controllers\Nexus\SectionController@latest')}}" class="text-white d-block py-3">
-                    <span class="h3 oi oi-pulse mr-1" aria-hidden="true" style="vertical-align:middle"></span> Latest
+                <a {!! App\Helpers\GoogleAnalyticsHelper::onClickEvent('BottomNavigation', 'Latest') !!} href="{{ action('App\Http\Controllers\Nexus\SectionController@latest') }}"
+                    class="text-white d-flex align-items-center justify-content-center py-3">
+                    <x-heroicon-s-bolt class="icon_medium mr-1" aria-hidden="true" />Latest
                 </a>
             </div>
 
         </div>
     </nav>
 </footer>
-
