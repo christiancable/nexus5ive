bf04f0ed01ce059804f6317ffaa05873c2dbfb4b
Christian Cable <christiancable@gmail.com>
2024-10-06 12:01:35 +0100

    don't bother searching if there's fewer than three chars in the filter

--- app/Livewire/Userlist.php
+++ app/Livewire/Userlist.php
@@ -4,7 +4,6 @@ namespace App\Livewire;
 
 use App\Models\User;
 use Livewire\Attributes\Computed;
-// use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class Userlist extends Component
@@ -14,12 +13,20 @@ class Userlist extends Component
     #[Computed]
     public function users()
     {
-        return User::select('username', 'name', 'popname', 'latestLogin', 'totalPosts', 'totalVisits')
-            ->verified()
-            ->where('username', 'like', '%'.$this->search.'%')
-            ->orWhere('name', 'like', '%'.$this->search.'%')
-            ->orderBy('username', 'asc')
-            ->get();
+        if (strlen($this->search) > 2)
+            {
+                return User::select('username', 'name', 'popname', 'latestLogin', 'totalPosts', 'totalVisits')
+                    ->verified()
+                    ->where('username', 'like', '%'.$this->search.'%')
+                    ->orWhere('name', 'like', '%'.$this->search.'%')
+                    ->orderBy('username', 'asc')
+                    ->get();
+            } else {
+                return User::select('username', 'name', 'popname', 'latestLogin', 'totalPosts', 'totalVisits')
+                    ->verified()
+                    ->orderBy('username', 'asc')
+                    ->get();
+            }
     }
 
     public function render()
