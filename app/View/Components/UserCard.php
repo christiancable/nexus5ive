828716c54bdd305e66c410239184018841a03e80
Christian Cable <christiancable@gmail.com>
2024-10-06 12:23:15 +0100

    add in formatting for user card depending on their activities

--- app/View/Components/UserCard.php
+++ app/View/Components/UserCard.php
@@ -16,6 +16,54 @@ class UserCard extends Component
         //
     }
 
+    public function formatDate($date): string
+    {
+        return $date->isoFormat('dddd DD MMMM YYYY [at] HH:mm');
+    }
+
+    public function classy($score): string
+    {
+        if ($score < 10) {
+            return 'text-secondary';
+        }
+
+        if ($score < 100) {
+            return 'text-dark';
+        }
+
+        if ($score < 1000) {
+            return 'text-info';
+        }
+
+        if ($score < 10000) {
+            return 'text-primary';
+        }
+
+        if ($score < 100000) {
+            return 'text-success';
+        }
+
+        return 'text-danger';
+    }
+
+    public function headingBackground($score): string
+    {
+        if ($score < 1) {
+            return 'bg-light';
+        }
+
+        return 'bg-info';
+    }
+
+    public function headingForeground($score): string
+    {
+        if ($score < 1) {
+            return 'text-secondary';
+        }
+
+        return 'text-white';
+    }
+
     /**
      * Get the view / contents that represent the component.
      */
