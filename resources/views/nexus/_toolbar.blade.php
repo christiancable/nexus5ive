f1e594ea682f0d530931a9efff2879c466d9a2c6
Christian Cable <christiancable@gmail.com>
2024-10-13 16:35:22 +0100

    update mobile notifications count to use user attribute

--- resources/views/nexus/_toolbar.blade.php
+++ resources/views/nexus/_toolbar.blade.php
@@ -1,13 +1,3 @@
-<?php
-$authUser = App\Models\User::withCount('sections')->findOrfail(Auth::id());
-$sectionsCount = $authUser->sections_count;
-
-$commentsCount = $authUser->newCommentCount();
-$messagesCount = $authUser->newMessageCount();
-
-$profileNotificationCount = $commentsCount + $messagesCount;
-$notificationCount = $profileNotificationCount;
-?>
 <div id="top-toolbar" class="border-bottom mb-3">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary navbar-transparent">
         <div class="container">
@@ -19,6 +9,9 @@ $notificationCount = $profileNotificationCount;
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar"
                 aria-expanded="true" aria-controls="navbar">
                 <span class="navbar-toggler-icon"></span>
+                @php
+                    $notificationCount = auth()->user()->notificationCount();
+                @endphp
                 @if ($notificationCount > 0)
                     <span class="badge badge-danger" id="notification-count">{{ $notificationCount }}</span>
                 @else
@@ -69,66 +62,9 @@ $notificationCount = $profileNotificationCount;
                     </li>
                 </ul>
 
-
                 <livewire:mentions />
-            
-                <ul class="nav navbar-nav">
-                    <li class="dropdown nav-item">
+                <livewire:profileMenu />
 
-                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false" id="profiledropdown">
-                            {{ $authUser->username }} &ndash; {{ $authUser->popname }}
-                            @if ($profileNotificationCount)
-                                <span class="badge badge-danger">{{ $profileNotificationCount }}</span>
-                            @endif
-                        </a>
-
-                        <div class="dropdown-menu" aria-labelledby="profiledropdown">
-                            <a class="dropdown-item"
-                                href="{{ action('App\Http\Controllers\Nexus\UserController@show', ['user' => $authUser->username]) }}">
-                                <x-heroicon-s-user class="icon_mini mr-1" aria-hidden="true" />Profile
-                                @if ($commentsCount)
-                                    <span class="badge badge-info">{{ $commentsCount }}</span>
-                                @endif
-                            </a>
-
-                            <a class="dropdown-item"
-                                href="{{ action('App\Http\Controllers\Nexus\ChatController@index') }}">
-                                <x-heroicon-s-chat-bubble-left-right class="icon_mini mr-1"
-                                    aria-hidden="true" />Messages
-                                @if ($messagesCount)
-                                    <span class="badge badge-info">{{ $messagesCount }}</span>
-                                @endif
-                            </a>
-
-                            @if ($authUser->administrator)
-                                <div role="separator" class="dropdown-divider"></div>
-                                <div class="dropdown-header dropdown-item">Administrator Goodies</div>
-                                <a class="dropdown-item"
-                                    href="{{ action('App\Http\Controllers\Nexus\ModeController@index') }}">
-                                    <x-heroicon-s-wrench class="icon_mini mr-1" aria-hidden="true" />BBS Settings
-                                </a>
-                            @endif
-
-                            @if ($sectionsCount)
-                                <div role="separator" class="dropdown-divider"></div>
-                                <div class="dropdown-header dropdown-item">Moderator Goodies</div>
-                                <a class="dropdown-item"
-                                    href="{{ action('App\Http\Controllers\Nexus\RestoreController@index') }}">
-                                    <x-heroicon-s-archive-box class="icon_mini mr-1" aria-hidden="true" />Your Archive
-                                </a>
-                            @endif
-
-
-                            <div role="separator" class="dropdown-divider"></div>
-                            <form class="form-inline" action="{{ url('/logout') }}" method="POST">
-                                @csrf
-                                <button class="btn btn-link dropdown-item"><x-heroicon-s-arrow-left-start-on-rectangle
-                                        class="icon_mini mr-1" aria-hidden="true" />Logout</button>
-                            </form>
-                        </div>
-                    </li>
-                </ul>
             </div>
         </div>
     </nav>
