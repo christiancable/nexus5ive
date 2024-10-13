ea5f8bf00232485eef1870b0d93bd0686135a884
Christian Cable <christiancable@gmail.com>
2024-10-13 14:18:20 +0100

    livewire component for mention notifications - remove old mentions controller

---
+++ app/Livewire/Mentions.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Livewire;
+
+use Illuminate\Http\Request;
+use Livewire\Component;
+
+class Notifications extends Component
+{
+    public $mentionsCount;
+
+    public $mentions = [];
+
+    public $user;
+
+    public $pollingInterval = 5;
+
+    public function mount(Request $request)
+    {
+        $this->pollingInterval = config('nexus.notification_check_interval');
+        $this->user = $request->user();
+        $this->fetchMentions();
+    }
+
+    public function fetchMentions()
+    {
+        $this->mentions = $this->user->mentions;
+        $this->mentionsCount = count($this->mentions);
+    }
+
+    public function clearMentions()
+    {
+        $this->user->clearMentions();
+        $this->fetchMentions();
+    }
+
+    public function render()
+    {
+        return view('livewire.notifications');
+    }
+}
