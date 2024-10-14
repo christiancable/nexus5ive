6220ae145e000af6a2b8af15a8a7bb3242ece5c6
Christian Cable <christiancable@gmail.com>
2024-10-14 08:18:01 +0100

    WIP - not working, not sure why

--- app/Livewire/Userlist.php
+++ app/Livewire/Userlist.php
@@ -3,30 +3,51 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Illuminate\Support\Collection;
 use Livewire\Attributes\Computed;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class Userlist extends Component
 {
-    public $search = '';
+    public string $search = '';
+
+    public Collection $allUsers;
+
+    public function mount()
+    {
+        $this->fetchUsers();
+    }
+
+    public function fetchUsers()
+    {
+        $seconds = 5 * 60;
+
+        $this->allUsers = Cache::remember('userslist', $seconds, function () {
+            return User::select('username', 'name', 'popname', 'latestLogin', 'totalPosts', 'totalVisits')
+                ->verified()
+                ->orderBy('username', 'asc')
+                ->get();
+        });
+    }
 
     #[Computed]
     public function users()
     {
-        if (strlen($this->search) > 2)
-            {
-                return User::select('username', 'name', 'popname', 'latestLogin', 'totalPosts', 'totalVisits')
-                    ->verified()
-                    ->where('username', 'like', '%'.$this->search.'%')
-                    ->orWhere('name', 'like', '%'.$this->search.'%')
-                    ->orderBy('username', 'asc')
-                    ->get();
-            } else {
-                return User::select('username', 'name', 'popname', 'latestLogin', 'totalPosts', 'totalVisits')
-                    ->verified()
-                    ->orderBy('username', 'asc')
-                    ->get();
-            }
+        $results = collect();
+
+        if (strlen($this->search) > 2) {
+            $results = $this->allUsers->filter(function ($key, $item) {
+                return true;
+                // return Str::contains($item['username'] . ' ' . $item['name'], $this->search, ignoreCase: true);
+            });
+        } else {
+            $results = $this->allUsers;
+        }
+
+        logger('Search term: ' . $this->search . ' Results count: ' . $results->count());
+        return $results;
     }
 
     public function render()
