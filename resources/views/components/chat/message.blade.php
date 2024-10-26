3c909653405181a4e6a52a9a5d59f9a21c4756ef
Christian Cable <christiancable@gmail.com>
2024-10-26 19:12:08 +0100

    show correct date hint

--- resources/views/components/chat/message.blade.php
+++ resources/views/components/chat/message.blade.php
@@ -1,7 +1,7 @@
 @props(['message'])
 
 <div class="d-flex mb-4 {{ $message['sender_id'] == auth()->id() ? 'justify-content-end' : 'justify-content-start' }}"
-    title="{{ \Carbon\Carbon::parse($message['time'])->diffForHumans() }}">
+    title="{{ \Carbon\Carbon::parse($message['created_at'])->diffForHumans() }}">
 
     <div class="px-3 py-2 {{ $message['sender_id'] == auth()->id() ? 'msg_author ' : 'msg_receiver' }} mb-n1">
         {!! Str::chopEnd(Str::chopStart(App\Helpers\NxCodeHelper::nxDecode($message['message_text']), '<p>'),'</p>') !!}
