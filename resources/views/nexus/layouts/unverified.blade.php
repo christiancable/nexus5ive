d70ac973c1066e56bce334bdf87fd2d6be683749
Christian Cable <christiancable@gmail.com>
2024-10-29 15:12:28 +0000

    updatre unverified layout to use the BBS mode rather than the old .env value

--- resources/views/nexus/layouts/unverified.blade.php
+++ resources/views/nexus/layouts/unverified.blade.php
@@ -1,56 +1,42 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1"> 
-    @yield('meta')
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" href="/apple-touch.png">
 
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+    @vite(['resources/sass/legacy.scss'])
+    @livewireStyles
     @vite(['resources/js/app.js'])
 
-    <?php
-    // if we have a special event then any theme it has should override the others
-    if (config('nexus.special_event') !== '') {
-        $specialTheme = App\Theme::where('name', '=', config('nexus.special_event'))->first();
-    } else {
-        $specialTheme = false;
-    }
-    ?>
+    @yield('meta')
+    @include('nexus._mode')
 
-    @if ($specialTheme)
-    <link rel="stylesheet" href="{{ asset($specialTheme->path) }}">
-    @else 
-        @if (config('nexus.bootstrap_theme'))
-                <link rel="stylesheet" href="{{config('nexus.bootstrap_theme')}}">
-                @vite(['resources/sass/extra.scss'])
-        @else
-            @vite(['resources/css/app.css'])
-  
-            
-        @endif
-    @endif 
+</head>
 
-
-    <link rel="apple-touch-icon" href="/apple-touch.png">
-  </head>
-
-  <body>
+<body>
     <div>
         
-        @include('nexus._toolbar-guest', ['hideRegistration' => true])
+        @include('nexus._toolbar-guest')
         
+
         @yield('breadcrumbs')
 
         @include('nexus._alerts')
 
         @yield('content')
 
-
         @include('nexus._googleanaytics')
     </div>
-  </body>
+    @livewireScripts
+</body>
+
 </html>
