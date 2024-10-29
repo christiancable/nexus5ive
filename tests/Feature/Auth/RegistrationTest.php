16fbac119d4161ffae613b2ebbd1c282a5064973
Christian Cable <christiancable@gmail.com>
2024-10-29 16:43:57 +0000

    update test to user username in the registration form

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -19,7 +19,7 @@ class RegistrationTest extends TestCase
     public function test_new_users_can_register(): void
     {
         $response = $this->post('/register', [
-            'name' => 'Test User',
+            'username' => 'TestUser',
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
