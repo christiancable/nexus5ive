8f54acaf38885c03ad0c2754a6b36884712178a7
Christian Cable <christiancable@gmail.com>
2024-10-29 15:12:48 +0000

    auth layouts for register and verify email

--- resources/views/nexus/auth/verify-email.blade.php
+++ resources/views/nexus/auth/verify-email.blade.php
@@ -1,32 +1,44 @@
-@extends('layouts.unverified')
+@extends('nexus.layouts.unverified')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                    <div class="card-body">
+
+                        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+
+                        @if (session('status') == 'verification-link-sent')
+                            <div class="alert alert-success mt-2" role="alert">
+                                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                            </div>
+                        @endif
+
+
+                        <div class="mt-4">
+
+                            <div class="d-flex justify-content-between">
+                                <form method="POST" action="{{ route('verification.send') }}">
+                                    @csrf
+                                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">
+                                            {{ __('Resend Verification Email') }}
+                                        </button>
+                                </form>
+
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button type="submit" class="btn btn-link p-0 m-0 align-baseline">
+                                        {{ __('Log Out') }}
+                                    </button>
+                                </form>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">
-                            {{ __('click here to request another') }}
-                        </button>.
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
