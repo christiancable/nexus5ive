8d076094b57b63f0b1b08e67913dc2708fb640bb
Christian Cable <christiancable@gmail.com>
2024-11-18 11:36:45 +0000

    WIP - lots broken

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,18 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/legacy.scss', // legacy bootstrap
-                'resources/sass/extra.scss', // legacy extras
+                // 'resources/sass/extra.scss', // legacy extras
                 'resources/css/app.css', // default laravel, unused
                 'resources/js/app.js',
 
                 // homegrown themes
                 'resources/sass/app.scss',
-                'resources/sass/excelsior.scss',
-                'resources/sass/extra.scss',
-                'resources/sass/halloween.scss',
-                'resources/sass/hotdog.scss',
-                'resources/sass/xmas.scss',
-                'resources/sass/legacy.scss',
+                // 'resources/sass/excelsior.scss',
+                // 'resources/sass/extra.scss',
+                // 'resources/sass/halloween.scss',
+                // 'resources/sass/hotdog.scss',
+                // 'resources/sass/xmas.scss',
+                // 'resources/sass/legacy.scss',
             ],
             refresh: true,
         }),
