6fdaed0343960fa98f505da502f3c6c49edd42f6
Christian Cable <christiancable@gmail.com>
2024-12-21 10:32:37 +0000

    take vite config back to framework defaults

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/legacy.scss', // legacy bootstrap
-                'resources/sass/extra.scss', // legacy extras
-                'resources/css/app.css', // default laravel, unused
-                'resources/js/app.js',
-
-                // homegrown themes
-                'resources/sass/app.scss',
-                'resources/sass/excelsior.scss',
-                'resources/sass/extra.scss',
-                'resources/sass/halloween.scss',
-                'resources/sass/hotdog.scss',
-                'resources/sass/xmas.scss',
-                'resources/sass/legacy.scss',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
