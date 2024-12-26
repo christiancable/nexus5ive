d8f36158d8423af9bea19a45302a7a2bd4a2b1e7
Christian Cable <christiancable@gmail.com>
2024-12-26 16:08:49 +0000

    position the clear comment button to the end of the comment row

--- resources/views/nexus/comments/_edit.blade.php
+++ resources/views/nexus/comments/_edit.blade.php
@@ -10,10 +10,13 @@
         @endif 
     </td>
     <td>
-        <form action="{{action('App\Http\Controllers\Nexus\CommentController@destroy', ['comment' => $comment->id])}}" method="POST">
+        <form action="{{action('App\Http\Controllers\Nexus\CommentController@destroy', ['comment' => $comment->id])}}" 
+            method="POST"
+            class="d-flex flex-row-reverse"
+            >
         @csrf
         @method('DELETE')
-            <button class="btn btn-danger">
+            <button class="btn btn-danger position-end">
             <x-heroicon-s-trash class="icon_mini mr-1" aria-hidden="true" />Delete</button>
         </form>
     </td>
