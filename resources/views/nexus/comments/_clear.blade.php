9bdd24bce4506e2fc2a8b1ea5f9948172cd372f6
Christian Cable <christiancable@gmail.com>
2024-12-26 15:54:11 +0000

    fix test by disabling smooth scrolling so I could scroll the button into view

--- resources/views/nexus/comments/_clear.blade.php
+++ resources/views/nexus/comments/_clear.blade.php
@@ -5,6 +5,6 @@
     <input type="hidden" name="redirect_user" value="{{ $user->username }}">
     
     <div class="mb-3">
-        <button type="submit" class="btn btn-danger form-control">Clear All Comments</button>
+        <button type="submit" class="btn btn-danger form-control" dusk="btn-clear-all-comments">Clear All Comments</button>
     </div>
 </form>
