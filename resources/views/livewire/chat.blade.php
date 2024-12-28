36ab0dcc36893c7dd444eaa5e51d4d6dbcb80a51
Christian Cable <christiancable@gmail.com>
2024-12-28 17:00:51 +0000

    to use h-100 all parents must have a size set

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -4,13 +4,10 @@
         <div class="col-md-4 col-xl-3 h-100 d-none d-md-block">
 
             <div class="card mb-sm-3 mb-md-0 h-100">
-                {{-- <div class="card-header">
-                    <h5 class="mb-0">Chats</h5>
-                </div> --}}
-                <div class="card-body chat_user_list_container p-0">
+                <div class="card-body chat_user_list_container p-0 h-100">
                     <ul class="list-group list-group-flush chat_user_list overflow-auto h-100" dusk="chat-list">
                         @foreach ($chats as $chat)
-                            <x-chat.user :user="$chat->partner" :chat_id="$chat->id" :active="$selectedChat && $selectedChat->id == $chat->id" :is_read="$chat->is_read"/>
+                            <x-chat.user :user="$chat->partner" :chat_id="$chat->id" :active="$selectedChat && $selectedChat->id == $chat->id" :is_read="$chat->is_read" />
                         @endforeach
                     </ul>
                 </div>
@@ -26,8 +23,8 @@
 
                     <x-chat.user-header :user="$selectedUser" :latest_message="$messages->last()" :users="$users" />
 
-                    <div class="card-body p-0">
-                        <div class="pe-2 ps-2 overflow-auto" dusk="chat-messages">
+                    <div class="card-body p-0 h-100">
+                        <div class="pe-2 ps-2 overflow-auto h-100 d-flex flex-column-reverse" dusk="chat-messages">
                             @foreach ($messages->reverse() as $message)
                                 <x-chat.message :message="$message" />
                             @endforeach
@@ -39,7 +36,8 @@
                             <input type="text" wire:model="newMessage" wire:keydown.enter="sendMessage"
                                 dusk="chat-input" class="form-control type_msg" placeholder="Type your message...">
                             <div class="input-group-append">
-                                <button wire:click="sendMessage" class="btn btn-primary send_btn" type="button" dusk="chat-send-button">
+                                <button wire:click="sendMessage" class="btn btn-primary send_btn" type="button"
+                                    dusk="chat-send-button">
                                     <x-heroicon-s-paper-airplane class="icon_mini" />
                                 </button>
                             </div>
@@ -54,7 +52,7 @@
                             <label class="visually-hidden" for="usersDropdown">Chat to&hellip;</label>
                             <select class="form-select" id="usersDropdown" wire:model="newChatUser"
                                 wire:change="selectUser($event.target.value)">
-                                    <option value="null" disabled>Chat with...</option>
+                                <option value="null" disabled>Chat with...</option>
                                 @foreach ($users as $user)
                                     <option value="{{ $user->id }}">{{ $user->username }}</option>
                                 @endforeach
