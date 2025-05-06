b020a55660237be081b33a874439e30ef7d818ee
Christian Cable <christiancable@gmail.com>
2025-05-06 15:38:38 +0100

    add moderation notes for reports

---
+++ app/Models/ModerationNote.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ModerationNote extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'report_id',
+        'user_id',
+        'user_name',
+        'note',
+    ];
+
+    // Relationships
+    public function report()
+    {
+        return $this->belongsTo(Report::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class)->withDefault();
+    }
+}
